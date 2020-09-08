d478f34d8aaafa1fcf7b596caaa40fbc2c565112
John Vinson <jtv3@uw.edu>
2020-09-08 12:37:27 -0400

    Initial check-in

---
+++ runScripts/Xspectra/makeXspectraInputs.py
@@ -0,0 +1,211 @@
+# John Vinson, 2020
+# based on earlier work by Sencer
+"""
+Make all the xspectra/qe inputs
+"""
+from ase.atoms import Atoms
+import ase.io.espresso
+from ase.io import write
+import spglib
+import pathlib
+from os import environ as env
+import sys
+import numpy as np
+from photonSym import photonSymm
+import json
+
+
+def smaller(atoms: Atoms, Rmin=9.0):
+#    # starting from the primitive cell, give a supercell
+#    # that has at least 9 Å in each direction; either using
+#    # primitive or conventinal cell as building blocks;
+#    # returns which ever is smaller
+    prim =  atoms * ((Rmin / np.linalg.norm(atoms.cell, axis=1)).astype(int) + 1)
+    lat, pos, Z = spglib.standardize_cell((atoms.get_cell(),
+                                           atoms.get_scaled_positions(),
+                                           atoms.get_atomic_numbers()))
+    conv = Atoms(Z, cell=lat, positions=pos@lat, pbc=True)
+    conv = conv * ((Rmin / np.linalg.norm(conv.cell, axis=1)).astype(int) + 1)
+    return conv if len(conv) <= len(prim) else prim
+
+
+def ortho(lat, i, j):
+    o = np.linalg.solve(lat.T, np.cross(lat[i], lat[j]))
+    return tuple(o / np.linalg.norm(o))
+
+def xinput(mode, iabs, dirs, xkvec, XSparams: dict, plot=False):
+
+    inp =  ["&input_xspectra",
+            "    calculation = 'xanes_%s'" % mode,
+            "    edge = '" + XSparams['input_xspectra']['edge'] + "'",
+            "    prefix = 'pwscf'",
+            "    outdir = '../'",
+            "    xniter = " + str(XSparams['input_xspectra']['xniter']),
+            "    xiabs = %d" % iabs,
+            "    xerror = " + str(XSparams['input_xspectra']['xerror']),
+            "    wf_collect = .true.",
+            "    xcheck_conv = " + str(XSparams['input_xspectra']['xcheck_conv']),
+            "    xepsilon(%d) = 1.0" % dirs[0],
+            "    xepsilon(%d) = 0.0" % dirs[1],
+            "    xepsilon(%d) = 0.0" % dirs[2]]
+
+    if mode == "quadrupole":
+
+        inp += ["    xkvec(1) = %.10f" % xkvec[0],
+                "    xkvec(2) = %.10f" % xkvec[1],
+                "    xkvec(3) = %.10f" % xkvec[2] ]
+
+    if plot:
+        inp += ["    xonly_plot = .true."]
+
+    inp += ["/",
+            "&plot",
+            "    xnepoint = " + str(XSparams['plot']['xnepoint']),
+            "    xemin = " + str(XSparams['plot']['xemin']),
+            "    xemax = " + str(XSparams['plot']['xemax']),
+            "    terminator = " + XSparams['plot']['terminator'],
+            "    cut_occ_states = " + XSparams['plot']['cut_occ_states'] ]
+    if plot:
+        inp += [
+            "    xgamma = " + str(XSparams['plotTrue']['xgamma']),
+            "    gamma_mode = '" + XSparams['plotTrue']['gamma_mode'] + "'",
+            "/"]
+    else:
+        inp += [
+            "    gamma_mode = '" + XSparams['plotFalse']['gamma_mode'] + "'",
+            "    gamma_energy(1) = " + str( XSparams['plotFalse']['gamma_energy(1)']),
+            "    gamma_energy(2) = " + str( XSparams['plotFalse']['gamma_energy(2)']),
+            "    gamma_value(1)  = " + str( XSparams['plotFalse']['gamma_value(1)']),
+            "    gamma_value(2)  = " + str( XSparams['plotFalse']['gamma_value(2)']),
+            "/"]
+
+    inp += ["&pseudos",
+            "    filecore = '../../../Ti.wfc'",
+            "/",
+            "&cut_occ",
+            "    cut_desmooth = " + str( XSparams['cut_occ']['cut_desmooth']),
+            "/",
+            XSparams['kpts']['kpts'] + " " + XSparams['kpts']['shift'] ]
+    return '\n'.join(inp) + '\n'
+
+
+
+def makeXspectra( mpid, unitCell: Atoms, params: dict ):
+    #######
+    psp = dict(Ti1='Ti.fch.upf')
+    symTarg = 'Ti'
+    ####
+
+    with open ("Xspectra/xspectra.json", 'r') as fd:
+        xsJSON = json.load(fd)
+
+    atoms = smaller( unitCell )
+
+    us = {}
+    ph = ()
+    photonSymm( atoms, us, ph, params['photonOrder'])
+
+
+    symm= spglib.get_symmetry((atoms.get_cell(),
+                             atoms.get_scaled_positions(),
+                             atoms.get_atomic_numbers()),
+                             symprec=0.1, angle_tolerance=15)
+
+    equiv = symm['equivalent_atoms']
+
+    symbols = atoms.get_chemical_symbols()
+
+
+    xsJSON['QE']['electrons']['conv_thr'] = params['defaultConvPerAtom'] * len( symbols )
+
+    with open ('Xspectra/SSSP_precision.json', 'r' ) as pspDatabaseFile:
+        pspDatabase = json.load( pspDatabaseFile )
+    minSymbols = set( symbols )
+    for symbol in minSymbols:
+        print( symbol )
+        print( pspDatabase[ symbol ]['filename'] )
+        psp[symbol] = pspDatabase[ symbol ]['filename']
+        if xsJSON['QE']['system']['ecutwfc'] < pspDatabase[ symbol ]['cutoff']:
+            xsJSON['QE']['system']['ecutwfc'] = pspDatabase[ symbol ]['cutoff']
+        if xsJSON['QE']['system']['ecutrho'] < pspDatabase[ symbol ]['rho_cutoff']:
+            xsJSON['QE']['system']['ecutrho'] = pspDatabase[ symbol ]['rho_cutoff']
+
+
+
+#    input = xsJSON['QE']
+    folder = pathlib.Path(env['PWD']) / mpid 
+    folder.mkdir(parents=True, exist_ok=True)
+    try:
+        write(str(folder / "qe.in"), atoms, format='espresso-in',
+            input_data=xsJSON['QE'], pseudopotentials=psp, kpts=[1, 1, 1])
+    except:
+        print(xsJSON['QE'], atoms, psp)
+        raise Exception("FAILED while trying to write qe.in")
+
+    iabs_ = 0
+    iabs = []
+    found = set()
+
+    for symbol in symbols:
+        if symbol == symTarg:
+            iabs.append(len(found) + 1)
+        else:
+            iabs.append(None)
+        found.add(symbol)
+
+    prev = None
+    XY = np.array(([0, 1, 0], [1, 0, 0], [0, 0, 1]))
+    XZ = np.array(([0, 0, 1], [0, 1, 0], [1, 0, 0]))
+    YZ = np.array(([1, 0, 0], [0, 0, 1], [0, 1, 0]))
+
+    directions = {1, 2, 3}
+
+
+    for i, sym in enumerate(symbols):
+
+      if i == equiv[i] and sym == symTarg:
+
+          if prev is not None:
+              atoms[prev].tag = 0
+
+          atoms[i].tag = 1
+          prev = i
+
+          subfolder = folder / str(i)
+          subfolder.mkdir(parents=True, exist_ok=True)
+
+          write(str(subfolder / "input.txt"), atoms, format='espresso-in',
+              input_data=xsJSON['QE'], pseudopotentials=psp, kpts=[1, 1, 1])
+
+          for dir in (1, 2, 3):
+              dir1, dir2 = directions - set((dir, ))
+              dirs = (dir, dir1, dir2)
+
+              for mode in ("dipole", "quadrupole"):
+
+                  xanesfolder = subfolder / ("%s%d" % (mode, dir))
+
+                  if any([np.all(XY == rot) for rot in symm['rotations']]) and dir == 2:
+                      xanesfolder.symlink_to(("%s1"%mode))
+                      continue
+
+                  if any([np.all(XZ == rot) for rot in symm['rotations']]) and dir == 3:
+                      xanesfolder.symlink_to(("%s1"%mode))
+                      continue
+
+                  if any([np.all(YZ == rot) for rot in symm['rotations']]) and dir == 3:
+                      xanesfolder.symlink_to(("%s2"%mode))
+                      continue
+
+                  xanesfolder.mkdir(parents=True, exist_ok=True)
+
+                  with open(xanesfolder / "xanes.in", "w") as f:
+                      f.write(xinput(mode, iabs[i], dirs,
+                                     ortho(atoms.cell, dir - 1, 
+                                           (dir2 if dir==2 else dir1)-1), xsJSON['XS']))
+
+                  with open(xanesfolder / "xanes_.in", "w") as f:
+                      f.write(xinput(mode, iabs[i], dirs,
+                                     ortho(atoms.cell, dir - 1,
+                                           (dir2 if dir==2 else dir1)-1), xsJSON['XS'],
+                                     plot=True))
---
+++ runScripts/photonSym.py
@@ -0,0 +1,173 @@
+# John Vinson, 2020
+"""
+Determines set of photon polarization/q-direction along with symmetry unique atoms
+"""
+from ase.atoms import Atoms
+import spglib
+import pprint
+import numpy as np
+
+
+def BuildOh( n, oh, norm ):
+    w = 1
+    norm = 6
+    if n > 6:
+        w = 40
+    oh.append( [1,0,0,w] )
+    oh.append( [0,1,0,w] )
+    oh.append( [0,0,1,w] )
+    oh.append( [-1,0,0,w] )
+    oh.append( [0,-1,0,w] )
+    oh.append( [0,0,-1,w] )
+
+    if n < 6:
+        return
+    norm = 30
+
+    w = 2
+    if n > 6:
+        w = 32
+    oh.append( [1,1,0,w] )
+    oh.append( [1,-1,0,w] )
+    oh.append( [-1,1,0,w] )
+    oh.append( [-1,-1,0,w] )
+    oh.append( [1,0,1,w] )
+    oh.append( [1,0,-1,w] )
+    oh.append( [-1,0,1,w] )
+    oh.append( [-1,0,-1,w] )
+    oh.append( [0,1,1,w] )
+    oh.append( [0,1,-1,w] )
+    oh.append( [0,-1,1,w] )
+    oh.append( [0,-1,-1,w] )
+
+    if n < 8:
+      return
+    norm = 840
+
+    w = 27
+    oh.append( [1,1,1,w] )
+    oh.append( [-1,1,1,w] )
+    oh.append( [1,-1,1,w] )
+    oh.append( [1,1,-1,w] )
+    oh.append( [-1,-1,1,w] )
+    oh.append( [1,-1,-1,w] )
+    oh.append( [-1,1,-1,w] )
+    oh.append( [-1,-1,-1,w] )
+
+
+
+
+
+#def photonSym( mode: str, atoms: Atoms, uniqueSites: dict, photons: list ):
+def photonSymm( atoms: Atoms, uniqueSites: dict, photons: list, order=4 ):
+    # for now mode is assumed to be quad
+
+    uniqueSites = {}
+    photons = []
+
+    symm= spglib.get_symmetry((atoms.get_cell(),
+                           atoms.get_scaled_positions(),
+                           atoms.get_atomic_numbers()),
+                           symprec=0.1, angle_tolerance=15)
+
+    equiv = symm['equivalent_atoms']
+
+    for i in equiv:
+        if i in uniqueSites:
+            uniqueSites[i] = uniqueSites[i] + 1
+        else:
+            uniqueSites[i] = 1
+
+
+    print( uniqueSites )
+
+
+    ohList = []
+    norm = 0
+    BuildOh( order, ohList, norm )
+    oh1 = np.vstack( ohList )
+
+
+    finalDipoleList = []
+    weight = 0
+
+    symm['rotations'] = np.reshape( np.append(symm['rotations'], [[-1,0,0],[0,-1,0],[0,0,-1]] ), (-1,3,3) )
+
+#    for rot in symm['rotations']:
+#        print( rot)
+
+
+    for sym in oh1:
+#        print( sym )
+#        trimSym = np.ndarray((3,), buffer=sym, dtype='intc' )
+        trimSym = sym[0:3]
+        found = False
+#        print( "=========------------" )
+#        print( trimSym )
+#        exit()
+        for i, test in enumerate( finalDipoleList ):
+#            testSym = np.ndarray((3,), buffer=test, dtype='intc' )
+            testSym = test[0:3]
+#            print( "------------" )
+#            print( testSym, trimSym )
+            for rot in symm['rotations']:
+#                print( rot)
+#                rotSym = rot * trimSym 
+                rotSym = rot.dot( trimSym )
+#                print( rotSym )
+                if( np.all(rotSym == testSym )):
+                    found = True
+                    weight = sym[3]
+                    break
+            if found:
+                break
+
+        if not found:
+            finalDipoleList.append( sym )
+        else:
+            finalDipoleList[i][3] += weight
+
+
+    print( "Final dipoles" )
+    for test in finalDipoleList:
+        print( test )
+
+
+
+    for i, dip in enumerate( finalDipoleList ):
+        dipVec = dip[0:3]
+        weight = dip[3]
+
+        test = np.array( [1,0,0] )
+        if( np.all( test == dipVec ) ):
+            test = np.array( [0,1,0] )
+
+        quad1 = np.cross( dipVec, test )
+        quad2 = np.cross( dipVec, quad1 )
+        quad = np.append( quad1, quad2 )
+        quad = np.append( quad, np.add( quad1, quad2 ) )
+        quad = np.reshape( np.append( quad, np.subtract( quad1, quad2 ) ), (4,3))
+
+        tempQuadList = []
+        for q in quad:
+            found = False
+            for i, test in enumerate( tempQuadList ):
+                testSym = test[0:3]
+                for rot in symm['rotations']:
+                    rotSym = rot.dot( trimSym )
+                    if( np.all(rotSym == testSym )):
+                        found = True
+                        break
+                if found:
+                    break
+            if not found:
+                storeQuad = np.append( q, weight )
+                tempQuadList.append( storeQuad )
+            else:
+                tempQuadList[i][3] += weight
+
+#        for q in tempQuadList:
+#            print(q)
+        photons.append( { "dipole": dip, "quad": tempQuadList, "norm": norm } )
+
+        print( { "dipole": dip, "quad": tempQuadList } )
