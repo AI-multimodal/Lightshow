7f13b0495faa5c59261e52f6554a08a04548585f
John Vinson <jtv3@uw.edu>
2020-09-09 11:24:25 -0400

    Initial OCEAN file writing is functioning

--- runScripts/fetchSingle.py
+++ runScripts/fetchSingle.py
@@ -17,6 +17,7 @@ from math import exp
 
 from photonSym import photonSymm
 from Xspectra.makeXspectraInputs import makeXspectra
+from OCEAN.makeOceanInputs import makeOcean
 
 # The script takes a single, positive integer to grab a system from materials project
 if len(sys.argv) < 2 :
@@ -51,25 +52,30 @@ unitC = ase.get_atoms(mp.get_structure_by_material_id(mpid, conventional_unit_ce
 data = mp.query(criteria={"task_id": mpid}, properties=["diel","band_gap"])
 print( data[0] )
 
+params = dict(defaultConvPerAtom=1E-10, photonOrder=6)
 
 ## Update OCEAN dielectric constant with calculated value or band_gap inverse-like
 if  data[0]['diel'] is not None: 
     if data[0]['diel']['poly_electronic'] is not None:
         print( data[0]['diel']['poly_electronic'] )
-        ocean_input['diemac'] = data[0]['diel']['poly_electronic']
+#        ocean_input['diemac'] = data[0]['diel']['poly_electronic']
+        params['diemac'] = data[0]['diel']['poly_electronic']
         if data[0]['band_gap'] is not None:
             print( data[0]['band_gap'] )
             print( exp( 3.5/data[0]['band_gap'] ) )
 elif data[0]['band_gap'] is not None:
     print( data[0]['band_gap'] )
-    ocean_input['diemac'] = exp( 3.5/data[0]['band_gap'] )
-    print(ocean_input['diemac'])
+#    ocean_input['diemac'] = exp( 3.5/data[0]['band_gap'] )
+#    print(ocean_input['diemac'])
+    params['diemac'] = exp( 3.5/data[0]['band_gap'] )
+    print(params['diemac'])
 
 
 
-params = dict(defaultConvPerAtom=1E-10, photonOrder=6)
 
 makeXspectra( mpid, unitC, params )
+
+makeOcean( mpid, unitC, params )
 
 ###############
 ############## JTV !!!!
