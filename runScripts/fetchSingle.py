2067f020269b20e768badf6e63cee0e25ed7ab07
xiaqu <xhqu1981@gmail.com>
2020-09-11 15:56:36 -0400

    1) create local data folder 2) add downloading timestamp

--- runScripts/fetchSingle.py
+++ runScripts/fetchSingle.py
@@ -14,58 +14,68 @@ import json
 
 from pprint import pprint
 from math import exp
+import time
+from pymatgen.ext.matproj import TaskType
 
 from photonSym import photonSymm
 from Xspectra.makeXspectraInputs import makeXspectra
 from OCEAN.makeOceanInputs import makeOcean
 from EXCITING.makeExcitingInputs import makeExciting
 
-# The script takes a single, positive integer to grab a system from materials project
-if len(sys.argv) < 2 :
-    print( "Requires MP number" )
-    exit()
-else :
-    print( str(sys.argv) )
-    mpid = 'mp-' + sys.argv[1]
 
-# Your hashe materials project key needs to be in a file called mp.key
-with open('mp.key', 'r' ) as f:
-    mpkey = f.read()
-    mpkey = mpkey.strip()
+def main():
 
-# MP api handler
-#print( str(mpkey) )
-mp = MPRester( str(mpkey) )
+    # The script takes a single, positive integer to grab a system from materials project
+    if len(sys.argv) < 2 :
+        print( "Requires MP number" )
+        exit()
+    else :
+        print( str(sys.argv) )
+        mpid = 'mp-' + sys.argv[1]
 
+    # Your hashe materials project key needs to be in a file called mp.key
+    with open('mp.key', 'r' ) as f:
+        mpkey = f.read()
+        mpkey = mpkey.strip()
 
-unitC = ase.get_atoms(mp.get_structure_by_material_id(mpid, conventional_unit_cell=False))
+    # MP api handler
+    #print( str(mpkey) )
+    mp = MPRester( str(mpkey) )
 
-data = mp.query(criteria={"task_id": mpid}, properties=["diel","band_gap"])
-print( data[0] )
+    st = mp.get_structure_by_material_id(mpid, conventional_unit_cell=False)
+    st_dict = st.as_dict().copy()
+    st_dict["download_at"] = time.ctime()
+    with open(f"../data/mp_structures/{mpid}.json", 'w') as f:
+        json.dump(st_dict, f, indent=4, sort_keys=True)
+    unitC = ase.get_atoms(st)
 
-params = dict(defaultConvPerAtom=1E-10, photonOrder=6)
+    data = mp.query(criteria={"task_id": mpid}, properties=["diel","band_gap"])
+    print( data[0] )
 
-## Update OCEAN dielectric constant with calculated value or band_gap inverse-like
-if  data[0]['diel'] is not None: 
-    if data[0]['diel']['poly_electronic'] is not None:
-        print( data[0]['diel']['poly_electronic'] )
-        params['diemac'] = data[0]['diel']['poly_electronic']
-        if data[0]['band_gap'] is not None:
-            print( data[0]['band_gap'] )
-            print( exp( 3.5/data[0]['band_gap'] ) )
-elif data[0]['band_gap'] is not None:
-    print( data[0]['band_gap'] )
-    params['diemac'] = exp( 3.5/data[0]['band_gap'] )
-    print(params['diemac'])
+    params = dict(defaultConvPerAtom=1E-10, photonOrder=6)
 
-
-
-
-makeXspectra( mpid, unitC, params )
-
-makeOcean( mpid, unitC, params )
-
-makeExciting( mpid, unitC, params )
+    ## Update OCEAN dielectric constant with calculated value or band_gap inverse-like
+    if  data[0]['diel'] is not None:
+        if data[0]['diel']['poly_electronic'] is not None:
+            print( data[0]['diel']['poly_electronic'] )
+            params['diemac'] = data[0]['diel']['poly_electronic']
+            if data[0]['band_gap'] is not None:
+                print( data[0]['band_gap'] )
+                print( exp( 3.5/data[0]['band_gap'] ) )
+    elif data[0]['band_gap'] is not None:
+        print( data[0]['band_gap'] )
+        params['diemac'] = exp( 3.5/data[0]['band_gap'] )
+        print(params['diemac'])
 
 
-exit()
+
+
+    makeXspectra( mpid, unitC, params )
+
+    makeOcean( mpid, unitC, params )
+
+    makeExciting( mpid, unitC, params )
+
+
+if __name__ == '__main__':
+    main()
