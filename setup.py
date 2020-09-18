5454adfa053e51eb928d6f22f293b7ab9436e187
xiaqu <xhqu1981@gmail.com>
2020-09-18 11:02:21 -0400

    read requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
+
+with open("requirements.txt") as f_req:
+    required_list = [line.rstrip() for line in f_req.readlines()]
+
+
 setup(
     name='xanes_bench',
     version='0.01',
@@ -9,6 +14,8 @@ setup(
     author='xiaqu',
     author_email='john.vinson@nist.gov; dlu@bnl.gov; vorwerk@physik.hu-berlin.de; xiaqu@bnl.gov',
     description='Benchmark code and data for XANES Simulation',
+    python_requires='>=3.7',
+    install_requires=required_list,
     entry_points={
         "console_scripts": [
             "fetchSingle = runScripts.fetchSingle:main",
