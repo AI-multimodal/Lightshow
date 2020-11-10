d7f2a4104a8d0fbabe2aefaa6166373ad03ea866
Christian Vorwerk <christian.vorwerk@physik.hu-berlin.de>
2020-11-10 11:42:33 +0100

    Small changes in EXCITING json & make computeQErmsd executable

--- xanes_bench/computeQErmsd.py
+++ xanes_bench/computeQErmsd.py
@@ -11,106 +11,109 @@ OkptDict = dict()
 Ha_c2018 = np.float64( 27.211386245988 )
 
 
+def main():
+    tree = ET.parse( os.path.join( "./XS", "pwscf.save", "data-file-schema.xml" ) )
+    root = tree.getroot()
 
-tree = ET.parse( os.path.join( "./XS", "pwscf.save", "data-file-schema.xml" ) )
-root = tree.getroot()
-
-# We will want to convert from the QE format to "crystal" format"
-cellList = []
-cellList.append( root.findall('output')[0].find('atomic_structure').find('cell')[0].text.split() )
-cellList.append( root.findall('output')[0].find('atomic_structure').find('cell')[1].text.split() )
-cellList.append( root.findall('output')[0].find('atomic_structure').find('cell')[2].text.split() )
-acell = np.asarray( cellList, dtype=np.double )
-alat = np.float64( root.find('output').find('atomic_structure').get("alat") )
-
-
-# Go ahead and count electrons (occupied bands)
-nelectron = int(float( root.find('output').find('band_structure').find('nelec').text ))
-occBands = int( nelectron / 2 )
-
-# And grab total number of bands
-nband = int(root.find('output').find('band_structure').find('nbnd').text)
-print( "N_electron: ", nelectron, "Occupied: ", occBands, "Total bands: ", nband )
-
-delta = np.float64( 0.0000000000001 )
-
-# Now parse and store the first run
-for kpt in root.find('output').find('band_structure').findall('ks_energies'):
-    
-    kvec = np.asarray( kpt.find('k_point').text.split(), dtype=np.double )
-    kvecReal = acell.dot( kvec ) / alat
-    kvecString = '{:18.10f} {:18.10f} {:18.10f}'.format( kvecReal[0]+delta, kvecReal[1]+delta, kvecReal[2]+delta )
-    weight = kpt.find('k_point').get('weight')
-  
-    eigs = kpt.find('eigenvalues').text.split()
-
-    XSkptDict[kvecString] = dict()
-    XSkptDict[kvecString]["weight"] = weight
-    XSkptDict[kvecString]["eigenvalues"] = eigs
-    print( kvecString )
+    # We will want to convert from the QE format to "crystal" format"
+    cellList = []
+    cellList.append( root.findall('output')[0].find('atomic_structure').find('cell')[0].text.split() )
+    cellList.append( root.findall('output')[0].find('atomic_structure').find('cell')[1].text.split() )
+    cellList.append( root.findall('output')[0].find('atomic_structure').find('cell')[2].text.split() )
+    acell = np.asarray( cellList, dtype=np.double )
+    alat = np.float64( root.find('output').find('atomic_structure').get("alat") )
 
 
-# Now parse the second run
-tree = ET.parse( os.path.join( "./OCEAN", "pwscf.save", "data-file-schema.xml" ) )
-root = tree.getroot()
+    # Go ahead and count electrons (occupied bands)
+    nelectron = int(float( root.find('output').find('band_structure').find('nelec').text ))
+    occBands = int( nelectron / 2 )
 
-for kpt in root.find('output').find('band_structure').findall('ks_energies'):
-    
-    kvec = np.asarray( kpt.find('k_point').text.split(), dtype=np.double )
-    kvecReal = acell.dot( kvec ) / alat
-    kvecString = '{:18.10f} {:18.10f} {:18.10f}'.format( kvecReal[0]+delta, kvecReal[1]+delta, kvecReal[2]+delta )
-    weight = kpt.find('k_point').get('weight')
-  
-    eigs = kpt.find('eigenvalues').text.split()
+    # And grab total number of bands
+    nband = int(root.find('output').find('band_structure').find('nbnd').text)
+    print( "N_electron: ", nelectron, "Occupied: ", occBands, "Total bands: ", nband )
 
-    OkptDict[kvecString] = dict()
-    OkptDict[kvecString]["weight"] = weight
-    OkptDict[kvecString]["eigenvalues"] = eigs
+    delta = np.float64( 0.0000000000001 )
 
+    # Now parse and store the first run
+    for kpt in root.find('output').find('band_structure').findall('ks_energies'):
+        
+        kvec = np.asarray( kpt.find('k_point').text.split(), dtype=np.double )
+        kvecReal = acell.dot( kvec ) / alat
+        kvecString = '{:18.10f} {:18.10f} {:18.10f}'.format( kvecReal[0]+delta, kvecReal[1]+delta, kvecReal[2]+delta )
+        weight = kpt.find('k_point').get('weight')
+      
+        eigs = kpt.find('eigenvalues').text.split()
 
-    
-# First calculate the RMSD over the occupied states
-rmsd = np.float64( 0.0 )
-totalWeight = np.float64( 0.0 )
-
-for kpt in XSkptDict:
-    if kpt not in OkptDict:
-        print( "Incompatible k-point meshes!" )
-        exit()
-
-    weight = np.float64( XSkptDict[kpt]["weight"] )
-    totalWeight += weight
-#    print( weight, totalWeight )
-
-    XSeigs = np.asarray( XSkptDict[kpt]["eigenvalues"], dtype=np.float64 )
-    Oeigs = np.asarray( OkptDict[kpt]["eigenvalues"], dtype=np.float64 )
-
-    for j in range( occBands ):
-        rmsd += weight*(XSeigs[j]-Oeigs[j])**2
-
-rmsd = np.sqrt( rmsd / totalWeight / occBands )
-print( "Occupied :  ", rmsd, rmsd*Ha_c2018)
+        XSkptDict[kvecString] = dict()
+        XSkptDict[kvecString]["weight"] = weight
+        XSkptDict[kvecString]["eigenvalues"] = eigs
+        print( kvecString )
 
 
+    # Now parse the second run
+    tree = ET.parse( os.path.join( "./OCEAN", "pwscf.save", "data-file-schema.xml" ) )
+    root = tree.getroot()
 
-# Now calculate the RMSD over all the states
-rmsd = np.float64( 0.0 )
-totalWeight = np.float64( 0.0 )
+    for kpt in root.find('output').find('band_structure').findall('ks_energies'):
+        
+        kvec = np.asarray( kpt.find('k_point').text.split(), dtype=np.double )
+        kvecReal = acell.dot( kvec ) / alat
+        kvecString = '{:18.10f} {:18.10f} {:18.10f}'.format( kvecReal[0]+delta, kvecReal[1]+delta, kvecReal[2]+delta )
+        weight = kpt.find('k_point').get('weight')
+      
+        eigs = kpt.find('eigenvalues').text.split()
 
-for kpt in XSkptDict:
-    if kpt not in OkptDict:
-        print( "Incompatible k-point meshes!" )
-        exit()
+        OkptDict[kvecString] = dict()
+        OkptDict[kvecString]["weight"] = weight
+        OkptDict[kvecString]["eigenvalues"] = eigs
 
-    weight = np.float64( XSkptDict[kpt]["weight"] )
-    totalWeight += weight
-#    print( weight, totalWeight )
 
-    XSeigs = np.asarray( XSkptDict[kpt]["eigenvalues"], dtype=np.float64 )
-    Oeigs = np.asarray( OkptDict[kpt]["eigenvalues"], dtype=np.float64 )
+        
+    # First calculate the RMSD over the occupied states
+    rmsd = np.float64( 0.0 )
+    totalWeight = np.float64( 0.0 )
 
-    for j in range( len( Oeigs ) ):
-        rmsd += weight*(XSeigs[j]-Oeigs[j])**2
+    for kpt in XSkptDict:
+        if kpt not in OkptDict:
+            print( "Incompatible k-point meshes!" )
+            exit()
 
-rmsd = np.sqrt( rmsd / totalWeight / nband )
-print( "Full range: ", rmsd, rmsd*Ha_c2018 )
+        weight = np.float64( XSkptDict[kpt]["weight"] )
+        totalWeight += weight
+    #    print( weight, totalWeight )
+
+        XSeigs = np.asarray( XSkptDict[kpt]["eigenvalues"], dtype=np.float64 )
+        Oeigs = np.asarray( OkptDict[kpt]["eigenvalues"], dtype=np.float64 )
+
+        for j in range( occBands ):
+            rmsd += weight*(XSeigs[j]-Oeigs[j])**2
+
+    rmsd = np.sqrt( rmsd / totalWeight / occBands )
+    print( "Occupied :  ", rmsd, rmsd*Ha_c2018)
+
+
+
+    # Now calculate the RMSD over all the states
+    rmsd = np.float64( 0.0 )
+    totalWeight = np.float64( 0.0 )
+
+    for kpt in XSkptDict:
+        if kpt not in OkptDict:
+            print( "Incompatible k-point meshes!" )
+            exit()
+
+        weight = np.float64( XSkptDict[kpt]["weight"] )
+        totalWeight += weight
+    #    print( weight, totalWeight )
+
+        XSeigs = np.asarray( XSkptDict[kpt]["eigenvalues"], dtype=np.float64 )
+        Oeigs = np.asarray( OkptDict[kpt]["eigenvalues"], dtype=np.float64 )
+
+        for j in range( len( Oeigs ) ):
+            rmsd += weight*(XSeigs[j]-Oeigs[j])**2
+
+    rmsd = np.sqrt( rmsd / totalWeight / nband )
+    print( "Full range: ", rmsd, rmsd*Ha_c2018 )
+
+if __name__ == '__main__':
+    main()
