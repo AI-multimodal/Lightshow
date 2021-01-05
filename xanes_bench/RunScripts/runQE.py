11e1abc28ac8e4c98ece182ed2f918775b42fc85
John Vinson <jtv3@uw.edu>
2021-01-05 14:36:48 -0500

    Modified runQE to run all band segments. Added collectQE to grab save data and move it

--- xanes_bench/RunScripts/runQE.py
+++ xanes_bench/RunScripts/runQE.py
@@ -8,6 +8,7 @@ from functools import reduce
 import shutil
 import subprocess
 import math
+import glob
 
 def factors(n):    
     return sorted(list(reduce(list.__add__, 
@@ -140,95 +141,105 @@ def runPW( rundir: str, clusterJSON: dict ):
         shutil.rmtree('nscf.save')
     shutil.move('pwscf.save', 'nscf.save' )
 
-    # Copy everything that isn't a wfc file
-    if not os.path.exists('pwscf.save'):
-        os.makedirs('pwscf.save')
             
     # TODO loop over k-point paths, but for now, just run the first
-    if not os.path.isfile("nscf_band.1.in"):
-      print( "Failed to find nscf_band.1.in" )
-      exit()
-    for f in os.listdir('scf.save'):
-        if os.path.isfile(os.path.join('scf.save', f) ) and not re.search( "wfc\d+.dat", f ):
-            shutil.copyfile( os.path.join('scf.save', f), os.path.join('pwscf.save', f) )
+    bandList = glob.glob("nscf_band*.in")
+    print( "Run NSCF for {:d} band segments".format(len(bandList)))
+    bandIter = 0
+
+    for bandFile in bandList:
+        bandIter += 1
+#        if not os.path.isfile("nscf_band.1.in"):
+#          print( "Failed to find nscf_band.1.in" )
+#          exit()
+        bandOutFile = bandFile.replace( "in", "out" )
+
+        # Copy everything that isn't a wfc file
+        if not os.path.exists('pwscf.save'):
+            os.makedirs('pwscf.save')
+
+        for f in os.listdir('scf.save'):
+            if os.path.isfile(os.path.join('scf.save', f) ) and not re.search( "wfc\d+.dat", f ):
+                shutil.copyfile( os.path.join('scf.save', f), os.path.join('pwscf.save', f) )
 
 
-    pathlib.Path("pwscf.EXIT").touch()
-    print( "Test NSCF band 1")
-    run = subprocess.run([clusterJSON["para_prefix"], clusterJSON["task_flag"],"1", 
-                          pw,"-inp","nscf_band.1.in"],
-                        capture_output=True,text=True)
-    # Can't go wider than 1 process per band or everything breaks
-    KSstates = int(re.search('number of Kohn-Sham states=\s+(\d+)', run.stdout ).group(1))
-    if KSstates is None:
-        print( "Failed to parse number of Kohn-Sham states in nscf_band.1.in run")
-        exit()
-    if KSstates < 1 or KSstates > 1000000:
-        print( "Unrealistic number of Kohn-Sham states:", KSstate )
-    NKpoints = int(re.search('number of k points=\s+(\d+)', run.stdout ).group(1))
-    if NKpoints is None:
-        print( "Failed to parse number of k points in nscf_band.1.in run")
-        exit()
-    if NKpoints < 1 or NKpoints > 1000000:
-        print( "Unrealistic number of k points:", NKpoints )
-    print( "NSCF band 1 has {:d} k-points and {:d} bands.".format( NKpoints, KSstates ))
+        pathlib.Path("pwscf.EXIT").touch()
+        print( "Test NSCF band 1")
+        run = subprocess.run([clusterJSON["para_prefix"], clusterJSON["task_flag"],"1", 
+                              pw,"-inp",bandFile],
+                            capture_output=True,text=True)
+        # Can't go wider than 1 process per band or everything breaks
+        KSstates = int(re.search('number of Kohn-Sham states=\s+(\d+)', run.stdout ).group(1))
+        if KSstates is None:
+            print( "Failed to parse number of Kohn-Sham states in {:s} run".format(bandFile))
+            exit()
+        if KSstates < 1 or KSstates > 1000000:
+            print( "Unrealistic number of Kohn-Sham states:", KSstate )
+        NKpoints = int(re.search('number of k points=\s+(\d+)', run.stdout ).group(1))
+        if NKpoints is None:
+            print( "Failed to parse number of k points in {:s} run".format(bandFile))
+            exit()
+        if NKpoints < 1 or NKpoints > 1000000:
+            print( "Unrealistic number of k points:", NKpoints )
+        print( "NSCF band {:d} has {:d} k-points and {:d} bands.".format( bandIter, NKpoints, KSstates ))
 
-    #TODO memory checking to constrain the paralellization for spare mem
-    r = re.search('Estimated max dynamical RAM per process >\s+(\d+\.\d+)\s+(\w+)', run.stdout)
-    MemPerProcess = float(r.group(1))
-    memUnits = r.group(2)
-    if( memUnits == 'MB' ):
-        MemPerProcess /= 1024
-        memUnits = 'GB'
+        #TODO memory checking to constrain the paralellization for spare mem
+        r = re.search('Estimated max dynamical RAM per process >\s+(\d+\.\d+)\s+(\w+)', run.stdout)
+        MemPerProcess = float(r.group(1))
+        memUnits = r.group(2)
+        if( memUnits == 'MB' ):
+            MemPerProcess /= 1024
+            memUnits = 'GB'
 
-    kpointFactors = factors(NKpoints)
-    npool = 1
-    ncpus = clusterJSON["MPI"]
-    bestScore = 0.0
-    if clusterJSON["MPI"] in kpointFactors:
-        npool = clusterJSON["MPI"]
-        bestScore = 1.0
-        #TODO memcheck
-    else:
-        for tcpus in range(clusterJSON["MPI"],int(clusterJSON["MPI"]/2),-1):
-            if bestScore > (tcpus/clusterJSON["MPI"]):
-                break
-            for tPool in factors(tcpus):
-                # Will break if fewer than 1 band per processor in a pool
-                if tcpus/tPool > KSstates:
-                    continue
-                tScore = NKpoints/(tPool*math.ceil(NKpoints/tPool)) \
-                       * ( 1.0-0.02*tcpus/tPool ) * tcpus/clusterJSON["MPI"]
-                if tScore > bestScore:
-                    bestScore = tScore
-                    npool = tPool
-                    ncpus = tcpus
+        kpointFactors = factors(NKpoints)
+        npool = 1
+        ncpus = clusterJSON["MPI"]
+        bestScore = 0.0
+        if clusterJSON["MPI"] in kpointFactors:
+            npool = clusterJSON["MPI"]
+            bestScore = 1.0
+            #TODO memcheck
+        else:
+            for tcpus in range(clusterJSON["MPI"],int(clusterJSON["MPI"]/2),-1):
+                if bestScore > (tcpus/clusterJSON["MPI"]):
+                    break
+                for tPool in factors(tcpus):
+                    # Will break if fewer than 1 band per processor in a pool
+                    if tcpus/tPool > KSstates:
+                        continue
+                    tScore = NKpoints/(tPool*math.ceil(NKpoints/tPool)) \
+                           * ( 1.0-0.02*tcpus/tPool ) * tcpus/clusterJSON["MPI"]
+                    if tScore > bestScore:
+                        bestScore = tScore
+                        npool = tPool
+                        ncpus = tcpus
 
-    # The test run messes up the xml, might also mess with Hubbard files 
-    for f in os.listdir('scf.save'):
-        if os.path.isfile(os.path.join('scf.save', f) ) and not re.search( "wfc\d+.dat", f ):
-            shutil.copyfile( os.path.join('scf.save', f), os.path.join('pwscf.save', f) )
-    print( "Run NSCF band 1 with {:d} MPI tasks and {:d} pools".format( ncpus, npool ))
-    run = subprocess.run([clusterJSON["para_prefix"], clusterJSON["task_flag"],str(ncpus),
-                          pw,"-inp","nscf_band.1.in","-nk",str(npool)],
-                         capture_output=True,text=True)
-    # Write out the output file, and if non-zero, the errors
-    with open( 'nscf_band.1.out', 'w' ) as fd:
-        fd.write(run.stdout)
-    if len( run.stderr ) > 0 :
-        with open( 'nscf_band.1.err', 'w' ) as fd:
-            fd.write(run.stderr)
+        # The test run messes up the xml, might also mess with Hubbard files 
+        for f in os.listdir('scf.save'):
+            if os.path.isfile(os.path.join('scf.save', f) ) and not re.search( "wfc\d+.dat", f ):
+                shutil.copyfile( os.path.join('scf.save', f), os.path.join('pwscf.save', f) )
+        print( "Run NSCF band {:d} with {:d} MPI tasks and {:d} pools".format( bandIter, ncpus, npool ))
+        run = subprocess.run([clusterJSON["para_prefix"], clusterJSON["task_flag"],str(ncpus),
+                              pw,"-inp",bandFile,"-nk",str(npool)],
+                             capture_output=True,text=True)
+        # Write out the output file, and if non-zero, the errors
+        with open( bandOutFile, 'w' ) as fd:
+            fd.write(run.stdout)
+        if len( run.stderr ) > 0 :
+            with open( bandFile.replace( "in", "err" ), 'w' ) as fd:
+                fd.write(run.stderr)
 
-    # Quit if the SCF didn't run to the end
-    if not re.search('JOB DONE', run.stdout ):
-        print( 'non-SCF Band 1 did not complete correctly!' )
-        exit()
+        # Quit if the SCF didn't run to the end
+        if not re.search('JOB DONE', run.stdout ):
+            print( 'non-SCF Band {:d} did not complete correctly!'.format( bandIter) )
+            exit()
 
-    # Move NSCF run directory
-    if os.path.isdir('nscf_band.1.save'):
-        shutil.rmtree('nscf_band.1.save')
-    shutil.move('pwscf.save', 'nscf_band.1.save' )    
-    
+        # Move NSCF run directory
+        bandDir = bandFile.replace( "in", "save" )
+        if os.path.isdir(bandDir):
+            shutil.rmtree(bandDir)
+        shutil.move('pwscf.save', bandDir )    
+        
 
 def main():
 
