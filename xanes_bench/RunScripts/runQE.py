ca35a5e605eeb3e0c67fe02feda28df7eadf49a6
John Vinson <jtv3@uw.edu>
2021-01-07 14:44:42 -0500

    Additional work for doing memory checking for QE ground-state runs

--- xanes_bench/RunScripts/runQE.py
+++ xanes_bench/RunScripts/runQE.py
@@ -14,8 +14,86 @@ def factors(n):
     return sorted(list(reduce(list.__add__, 
                 ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0))))
 
+def parsePWforParal( stdout: str, runText: str, maxMem: int, maxCPU: int ):
+    poolEfficiency = 0.95
+    intraPoolEfficiency = 0.95
+
+    # Can't go wider than 1 process per band or everything breaks
+    KSstates = int(re.search('number of Kohn-Sham states=\s+(\d+)', stdout ).group(1))
+    if KSstates is None:
+        print( "Failed to parse number of Kohn-Sham states in nscf_band.1.in run")
+        exit()
+    if KSstates < 1 or KSstates > 1000000:
+        print( "Unrealistic number of Kohn-Sham states:", KSstate )
+    NKpoints = int(re.search('number of k points=\s+(\d+)', stdout ).group(1))
+    if NKpoints is None:
+        print( "Failed to parse number of k points in nscf_band.1.in run")
+        exit()
+    if NKpoints < 1 or NKpoints > 1000000:
+        print( "Unrealistic number of k points:", NKpoints )
+    print( "{:s} has {:d} k-points and {:d} bands.".format( runText, NKpoints, KSstates ))
+    #TODO memory checking to constrain the paralellization for spare mem
+    r = re.search('Estimated max dynamical RAM per process >\s+(\d+\.\d+)\s+(\w+)', stdout)
+    MemPerProcess = float(r.group(1))
+    memUnits = r.group(2)
+    if( memUnits == 'MB' ):
+        MemPerProcess /= 1024
+        memUnits = 'GB'
+
+    r = re.search('wfc\\ \\(w\\.\\ buffer\\):\s+(\d+\\.\d+)\s+(\w+)',stdout)
+    WfcMemPerProcess = float(r.group(1))
+    memUnits = r.group(2)
+    if( memUnits == 'MB' ):
+        WfcMemPerProcess /= 1024
+        memUnits = 'GB'
+
+    kpointFactors = factors(NKpoints)
+    npool = 1
+    ncpus = maxCPU
+    bestScore = 0.0
+
+#   First see if the number of kpoints is exactly divisible by the MPI processes
+    if maxCPU in kpointFactors:
+        # Then check memory
+        memGuess = ( MemPerProcess - WfcMemPerProcess ) \
+                 + WfcMemPerProcess * ( poolEfficiency/maxCPU + (1-poolEfficiency) )
+        if memGuess < maxMem:
+            npool = maxCPU
+            bestScore = 1.0
+            print( "Mem guess: {:f} GB".format( memGuess ))
+
+    if npool != maxCPU:
+# loop over total cpus to use from max to half
+# loop over cpu factors and calculate score
+#   Count the time it takes the slowest pool (most kpoints)
+#   small penalty for G parallel as opposed to k parallel
+#   % penalty for leaving out CPUs entirely
+#   Score is [ ceil(nkpoints/npools)*npools/nkpt]^-1 * ( 1-0.02*ncpus/npools) * ncpusUsed/ncpustot
+# keep best, and score, exit loop over ncpus when used/tot drops below best score:w
+        for tcpus in range(maxCPU,int(maxCPU/2),-1):
+            if bestScore > (tcpus/maxCPU):
+                break
+            for tPool in factors(tcpus):
+                # Will break if fewer than 1 band per processor in a pool
+                if tcpus/tPool > KSstates:
+                    continue
+                tScore = NKpoints/(tPool*math.ceil(NKpoints/tPool)) \
+                       * ( 1.0-0.02*tcpus/tPool ) * tcpus/maxCPU
+                if tScore > bestScore:
+                    memGuess = ( MemPerProcess - WfcMemPerProcess ) \
+                             + WfcMemPerProcess * ( poolEfficiency/tPool + (1-poolEfficiency) )
+                    memGuess *= ( intraPoolEfficiency*tPool/tcpus + (1-intraPoolEfficiency) )
+
+                    if memGuess < maxMem:
+                      bestScore = tScore
+                      npool = tPool
+                      ncpus = tcpus
+                      print( "Mem guess: {:f} GB".format( memGuess ))
+    return ncpus, npool;
+
 
 def runPW( rundir: str, clusterJSON: dict ):
+
     os.chdir( rundir )
     # Set PW.x path
     pw = pathlib.Path( clusterJSON["QE_BIN"], "pw.x" )
@@ -30,59 +108,8 @@ def runPW( rundir: str, clusterJSON: dict ):
     run = subprocess.run([clusterJSON["para_prefix"], clusterJSON["task_flag"],"1", pw,"-inp","scf.in"],
                         capture_output=True,text=True)
 
-    # Can't go wider than 1 process per band or everything breaks
-    KSstates = int(re.search('number of Kohn-Sham states=\s+(\d+)', run.stdout ).group(1))
-    if KSstates is None:
-        print( "Failed to parse number of Kohn-Sham states in nscf_band.1.in run")
-        exit()
-    if KSstates < 1 or KSstates > 1000000:
-        print( "Unrealistic number of Kohn-Sham states:", KSstate )
-    NKpoints = int(re.search('number of k points=\s+(\d+)', run.stdout ).group(1))
-    if NKpoints is None:
-        print( "Failed to parse number of k points in nscf_band.1.in run")
-        exit()
-    if NKpoints < 1 or NKpoints > 1000000:
-        print( "Unrealistic number of k points:", NKpoints )
-    print( "SCF has {:d} k-points and {:d} bands.".format( NKpoints, KSstates ))
-
-    #TODO memory checking to constrain the paralellization for spare mem
-    r = re.search('Estimated max dynamical RAM per process >\s+(\d+\.\d+)\s+(\w+)', run.stdout)
-    MemPerProcess = float(r.group(1))
-    memUnits = r.group(2)
-    if( memUnits == 'MB' ):
-        MemPerProcess /= 1024
-        memUnits = 'GB'
-
-    kpointFactors = factors(NKpoints)
-    npool = 1
-    ncpus = clusterJSON["MPI"]
-    bestScore = 0.0
-    if clusterJSON["MPI"] in kpointFactors:
-        npool = clusterJSON["MPI"]
-        bestScore = 1.0
-        #TODO memcheck
-    else:
-# loop over total cpus to use from max to half
-# loop over cpu factors and calculate score
-#   Count the time it takes the slowest pool (most kpoints)
-#   small penalty for G parallel as opposed to k parallel
-#   % penalty for leaving out CPUs entirely
-#   Score is [ ceil(nkpoints/npools)*npools/nkpt]^-1 * ( 1-0.02*ncpus/npools) * ncpusUsed/ncpustot
-# keep best, and score, exit loop over ncpus when used/tot drops below best score:w
-        for tcpus in range(clusterJSON["MPI"],int(clusterJSON["MPI"]/2),-1):
-            if bestScore > (tcpus/clusterJSON["MPI"]):
-                break
-            for tPool in factors(tcpus):
-                # Will break if fewer than 1 band per processor in a pool
-                if tcpus/tPool > KSstates:
-                    continue
-                tScore = NKpoints/(tPool*math.ceil(NKpoints/tPool)) \
-                       * ( 1.0-0.02*tcpus/tPool ) * tcpus/clusterJSON["MPI"]
-                if tScore > bestScore:
-                    bestScore = tScore
-                    npool = tPool
-                    ncpus = tcpus
-
+    # Set up parallelization for SCF
+    ncpus, npool = parsePWforParal( run.stdout, "SCF", clusterJSON["memory"], clusterJSON["MPI"] )
 
     # Run SCF calculation    
     print( "Run SCF with {:d} MPI tasks and {:d} pools".format( ncpus, npool ))
@@ -111,11 +138,22 @@ def runPW( rundir: str, clusterJSON: dict ):
     # Copy everything that isn't a wfc file
     if not os.path.exists('pwscf.save'):
         os.makedirs('pwscf.save')
-    # TODO, with mem checking we'll need to re-run the test case
-    # which will need to happen before we copy the density file over
     for f in os.listdir('scf.save'):
         if os.path.isfile(os.path.join('scf.save', f) ) and not re.search( "wfc\d+.dat", f ):
             shutil.copyfile( os.path.join('scf.save', f), os.path.join('pwscf.save', f) )
+
+    pathlib.Path("pwscf.EXIT").touch()
+    print( "Test nscf run")
+    run = subprocess.run([clusterJSON["para_prefix"], clusterJSON["task_flag"],"1",
+                          pw,"-inp","nscf.in"],capture_output=True,text=True)
+
+    # Set up parallelization for NSCF
+    ncpus, npool = parsePWforParal( run.stdout, "NSCF", clusterJSON["memory"], clusterJSON["MPI"] )
+
+    # re-copy data-file-schema.xml
+    shutil.copyfile( os.path.join('scf.save', 'data-file-schema.xml'),
+                     os.path.join('pwscf.save', 'data-file-schema.xml') )
+
 
     print( "Run NSCF with {:d} MPI tasks and {:d} pools".format( ncpus, npool ))
     run = subprocess.run([clusterJSON["para_prefix"], clusterJSON["task_flag"],str(ncpus),
@@ -149,9 +187,7 @@ def runPW( rundir: str, clusterJSON: dict ):
 
     for bandFile in bandList:
         bandIter += 1
-#        if not os.path.isfile("nscf_band.1.in"):
-#          print( "Failed to find nscf_band.1.in" )
-#          exit()
+
         bandOutFile = bandFile.replace( "in", "out" )
 
         # Copy everything that isn't a wfc file
@@ -168,51 +204,10 @@ def runPW( rundir: str, clusterJSON: dict ):
         run = subprocess.run([clusterJSON["para_prefix"], clusterJSON["task_flag"],"1", 
                               pw,"-inp",bandFile],
                             capture_output=True,text=True)
-        # Can't go wider than 1 process per band or everything breaks
-        KSstates = int(re.search('number of Kohn-Sham states=\s+(\d+)', run.stdout ).group(1))
-        if KSstates is None:
-            print( "Failed to parse number of Kohn-Sham states in {:s} run".format(bandFile))
-            exit()
-        if KSstates < 1 or KSstates > 1000000:
-            print( "Unrealistic number of Kohn-Sham states:", KSstate )
-        NKpoints = int(re.search('number of k points=\s+(\d+)', run.stdout ).group(1))
-        if NKpoints is None:
-            print( "Failed to parse number of k points in {:s} run".format(bandFile))
-            exit()
-        if NKpoints < 1 or NKpoints > 1000000:
-            print( "Unrealistic number of k points:", NKpoints )
-        print( "NSCF band {:d} has {:d} k-points and {:d} bands.".format( bandIter, NKpoints, KSstates ))
 
-        #TODO memory checking to constrain the paralellization for spare mem
-        r = re.search('Estimated max dynamical RAM per process >\s+(\d+\.\d+)\s+(\w+)', run.stdout)
-        MemPerProcess = float(r.group(1))
-        memUnits = r.group(2)
-        if( memUnits == 'MB' ):
-            MemPerProcess /= 1024
-            memUnits = 'GB'
-
-        kpointFactors = factors(NKpoints)
-        npool = 1
-        ncpus = clusterJSON["MPI"]
-        bestScore = 0.0
-        if clusterJSON["MPI"] in kpointFactors:
-            npool = clusterJSON["MPI"]
-            bestScore = 1.0
-            #TODO memcheck
-        else:
-            for tcpus in range(clusterJSON["MPI"],int(clusterJSON["MPI"]/2),-1):
-                if bestScore > (tcpus/clusterJSON["MPI"]):
-                    break
-                for tPool in factors(tcpus):
-                    # Will break if fewer than 1 band per processor in a pool
-                    if tcpus/tPool > KSstates:
-                        continue
-                    tScore = NKpoints/(tPool*math.ceil(NKpoints/tPool)) \
-                           * ( 1.0-0.02*tcpus/tPool ) * tcpus/clusterJSON["MPI"]
-                    if tScore > bestScore:
-                        bestScore = tScore
-                        npool = tPool
-                        ncpus = tcpus
+        # Set up parallelization for NSCF
+        runString = "Band {:d}".format( bandIter )
+        ncpus, npool = parsePWforParal( run.stdout, runString, clusterJSON["memory"], clusterJSON["MPI"] )
 
         # The test run messes up the xml, might also mess with Hubbard files 
         for f in os.listdir('scf.save'):
