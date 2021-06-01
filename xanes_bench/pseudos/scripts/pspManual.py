fc7bee32fd4cafe3f4d09f2469b171de457ee3b3
John Vinson <jtv3@uw.edu>
2021-06-01 11:53:05 -0400

    Added option to include wfc file too (for core-level)

--- xanes_bench/pseudos/scripts/pspManual.py
+++ xanes_bench/pseudos/scripts/pspManual.py
@@ -1,6 +1,7 @@
 import json
 import os
 import base64, bz2
+import hashlib
 
 
 
@@ -26,7 +27,54 @@ def pspAdd( js, pspjs ):
     rho = input( "What is the rho cutoff?")
     dual = float(rho)/float(cutoff)
     filename = input("What is the pseduo file name?")
+    pspCollection = input("What is the reference for the pseudo?")
+    saveWFC = input( "Save the wavefunction files too? (y/n) " )
+    wfcShell = []
+    wfcFile = []
+    if saveWFC[0] == 'y' or saveWFC[0] == 'Y':
+        while True:
+            ws = input( "Which core hole? (1s, 2p) (any other entry to exit)" )
+            if ws == '1s' or ws == '2p':
+                wfcShell.append( ws )
+                wfcFile.append( input("What is the wfc file name?") )
+
+            else:
+                break
+
+
+    with open( filename, 'r' ) as f:
+        pspString = f.read()
+
+    filename = os.path.basename( filename )
+    pspjs[filename] = base64.b64encode(bz2.compress(pspString.encode("utf-8"),9)).decode("utf-8")
+
+    md5hash = hashlib.md5( pspString.encode("utf-8") ).hexdigest()
+
+
+
+
+    if element not in js:
+        js[element] = {}
+    js[element]['Z_val'] = Z
+    js[element]['cutoff'] = cutoff
+    js[element]['dual'] = dual
+    js[element]['filename'] = filename
+    js[element]['md5'] = md5hash
+    js[element]['pseudopotential'] = pspCollection
+    js[element]['rho_cutoff'] = rho
   
+    if len( wfcShell ) == 0:
+        return True
+    if 'wfc' not in js[element]:
+        js[element]['wfc'] = {}
+    for i in range(len( wfcShell)):
+        with open( wfcFile[i], 'r' ) as f:
+            wfcString = f.read()
+
+        wfcFile[i] = os.path.basename( wfcFile[i] )
+        pspjs[ filename + '_' + wfcShell[i] ] = base64.b64encode(bz2.compress(wfcString.encode("utf-8"),9)).decode("utf-8")
+        wfchash = hashlib.md5( wfcString.encode("utf-8") ).hexdigest()
+        js[element]['wfc'][wfcShell[i]] = { 'md5': wfchash, 'file': filename + '_' + wfcShell[i] }
     return True
 
 
@@ -34,10 +82,14 @@ def main():
     dbRootName = input( "What is the database root?")
 
     
-    with open( dbRootName + '.json', 'r' ) as f:
-        js = json.load( f )
-    with open( dbRootName + '_pseudos.json', 'r' ) as f:
-        pspjs = json.load( f )
+    if os.path.isfile( dbRootName + '.json' ):
+        with open( dbRootName + '.json', 'r' ) as f:
+            js = json.load( f )
+        with open( dbRootName + '_pseudos.json', 'r' ) as f:
+            pspjs = json.load( f )
+    else:
+        js = {}
+        pspjs = {}
 
 
 
@@ -55,11 +107,11 @@ def main():
         c = 'q'
 
     print( update)
-    update = False
+
     if update:
         with open( dbRootName + '.json', 'w' ) as f:
             f.write( json.dumps( js ) )
-        with open( dbRootName + '_pseudos.json', 'r' ) as f:
+        with open( dbRootName + '_pseudos.json', 'w' ) as f:
             f.write( json.dumps( pspjs  ))
 
 
