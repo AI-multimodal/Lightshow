96ecd3265336c16bdebbc26de0e1074b7ba5717f
FCMeng <fnachem@clemson.edu>
2021-08-18 08:33:43 -0400

    rewrite the setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 
-with open("requirements.txt") as f_req:
-    required_list = [line.rstrip() for line in f_req.readlines()]
+#with open("requirements.txt") as f_req:
+#    required_list = [line.rstrip() for line in f_req.readlines()]
 
 
 setup(
@@ -15,7 +15,7 @@ setup(
     author_email='john.vinson@nist.gov; dlu@bnl.gov; vorwerk@physik.hu-berlin.de; xiaqu@bnl.gov',
     description='Benchmark code and data for XANES Simulation',
     python_requires='>=3.7',
-    install_requires=required_list,
+#    install_requires=required_list,
     entry_points={
         "console_scripts": [
             "fetchSingle = xanes_bench.fetchSingle:main",
