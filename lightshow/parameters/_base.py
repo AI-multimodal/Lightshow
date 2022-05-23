51f65bd2ae14ac4cd5686f616364f1c41867da56
Matthew Carbone <x94carbone@gmail.com>
2022-05-23 11:54:53 -0400

    Add name abstract property to _BaseParameters

--- lightshow/parameters/_base.py
+++ lightshow/parameters/_base.py
@@ -5,3 +5,8 @@ class _BaseParameters(ABC):
     @abstractmethod
     def write(self, target_directory, kwargs):
         ...
+
+    @property
+    @abstractmethod
+    def name(self):
+        return self._name
