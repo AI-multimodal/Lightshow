a50dfcecb452dc27d2d696c9b809fc0cb160f15f
Matthew Carbone <x94carbone@gmail.com>
2022-06-24 12:19:49 -0400

    Update code and add basic usage tutorial

--- lightshow/__init__.py
+++ lightshow/__init__.py
@@ -18,6 +18,18 @@ def _get_API_key_from_environ():
     return environ.get("PMG_API_KEY", None)
 
 
+def _get_POTCAR_DIRECTORY_from_environ():
+    """Checks for an environment variable VASP_POTCAR_DIRECTORY. If does not
+    exist, returns None.
+
+    Returns
+    -------
+    str
+    """
+
+    return environ.get("VASP_POTCAR_DIRECTORY", None)
+
+
 from lightshow.database import Database  # noqa
 from lightshow.parameters.feff import FEFFParameters  # noqa
 from lightshow.parameters.vasp import VASPParameters  # noqa
--- lightshow/defaults.py
+++ lightshow/defaults.py
@@ -1,6 +1,9 @@
 """Helper module for making the API a bit cleaner when dealing with default
 parameters."""
 
+# Note that pre-commit + .flake8 ignores this file since it's just a bunch of
+# imports...
+
 from lightshow.parameters.vasp import (
     VASP_INCAR_DEFAULT_NEUTRAL_POTENTIAL,
     VASP_INCAR_DEFAULT_COREHOLE_POTENTIAL,
