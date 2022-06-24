cc8f3dd88e21e91cac79d6eb55262ba5a64dc525
Matthew Carbone <x94carbone@gmail.com>
2022-06-24 11:11:22 -0400

    Make some quality of life improvements to the API

---
+++ lightshow/defaults.py
@@ -0,0 +1,8 @@
+"""Helper module for making the API a bit cleaner when dealing with default
+parameters."""
+
+from lightshow.parameters.vasp import (
+    VASP_INCAR_DEFAULT_NEUTRAL_POTENTIAL,
+    VASP_INCAR_DEFAULT_COREHOLE_POTENTIAL,
+    VASP_POTCAR_DEFAULT_ELEMENT_MAPPING,
+)
