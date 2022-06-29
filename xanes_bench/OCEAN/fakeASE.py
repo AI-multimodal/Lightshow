35c41ee595def2394e0ad1ae0747498b28321f5d
John Vinson <jtv3@uw.edu>
2022-06-29 13:17:03 +0200

    For compatibility with OCEAN v2, write explicit screen.nkpt

--- xanes_bench/OCEAN/fakeASE.py
+++ xanes_bench/OCEAN/fakeASE.py
@@ -8,14 +8,20 @@ from pymatgen.core import Structure
 #from collections import OrderedDict
 import os
 import sys
-from pymatgen.io.vasp.sets import MPStaticSet
+#from pymatgen.io.vasp.sets import MPStaticSet
 from ase.units import Bohr
+import numpy as np
 
 def write_ocean_in( filename: str, structure: Structure, input_data: dict ):
 
     filename = os.path.expanduser(filename)
     mode = 'w'
     fd = open(filename, mode)
+
+    #Change screen.nkpt -Int to triplet of ints
+    if 'screen.nkpt' in input_data and len(input_data['screen.nkpt'].split()) == 1:
+       input_data['screen.nkpt'] = oceanKptSampling( structure.lattice.matrix, float(input_data['screen.nkpt']) )
+
 
     input_data_str = []
     for key in input_data:
@@ -82,3 +88,17 @@ def write_ocean_in( filename: str, structure: Structure, input_data: dict ):
               '        {cell[2][0]:.14f} {cell[2][1]:.14f} {cell[2][2]:.14f}  }}\n'
                    ''.format(cell=structure.lattice.matrix))
 
+
+
+def oceanKptSampling( cell, kpt ):
+    # Should normalize each cell dim by Bohr, or we can just do it the once for volume
+    v = abs( np.dot(np.cross(cell[0], cell[1]), cell[2] ))/Bohr
+    b1 = 2*np.pi*np.linalg.norm(np.cross(cell[1], cell[2]))/v
+    b2 = 2*np.pi*np.linalg.norm(np.cross(cell[0], cell[2]))/v
+    b3 = 2*np.pi*np.linalg.norm(np.cross(cell[0], cell[1]))/v
+
+    k1 = int( -kpt * b1 ) + 1
+    k2 = int( -kpt * b2 ) + 1
+    k3 = int( -kpt * b3 ) + 1
+
+    return "{:d} {:d} {:d}".format( k1, k2, k3 )
