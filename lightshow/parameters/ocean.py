5e3e36b3757505c9dc14c209d2a6db4074968fdd
John Vinson <jtv3@uw.edu>
2022-07-20 15:30:53 +0200

    Removed OCEAN_DEFAULTS_CARD content as OCEAN keeps its own defaults. Updated the edge definition to support a range of edge options

--- lightshow/parameters/ocean.py
+++ lightshow/parameters/ocean.py
@@ -9,33 +9,7 @@ from ase.units import Bohr
 from lightshow.parameters._base import _BaseParameters
 
 
-OCEAN_DEFAULT_CARDS = {
-    "dft": "qe",
-    "dft_energy_range": 50,
-    "diemac": "5",
-    "ecut": "-1",
-    "ecut.quality": "high",
-    "edges": "-22 1 0",
-    "opf.program": "hamann",
-    "para_prefix": "mpirun -n 8",
-    "pp_database": "ONCVPSP-PBE-PDv0.4-stringent",
-    "screen_energy_range": 150,
-    "cnbse.broaden": 0.89,
-    "core_offset": "225.10",
-    "cnbse.spect_range": "1000 -20 80",
-    "screen.nkpt": "-2.55",
-    "screen.final.dr": "0.02",
-    "screen.grid.rmax": "10",
-    "screen.grid.rmode": "lagrange uniform",
-    "screen.grid.ang": "5 11 11 9 7",
-    "screen.grid.deltar": "0.10 0.15 0.25 0.25 0.25",
-    "screen.grid.shells": " -1 4 6 8 10",
-    "screen.lmax": "2",
-    "cnbse.rad": "5.5",
-    "screen.shells": "3.5 4.0 4.5 5.0 5.5 6.0",
-    "cnbse.niter": 1000,
-    "haydock_convergence": " 0.001 5 ",
-}
+OCEAN_DEFAULT_CARDS = {}
 
 
 class OCEANParameters(MSONable, _BaseParameters):
@@ -132,7 +106,33 @@ class OCEANParameters(MSONable, _BaseParameters):
     @property
     def _edge_map(self):
         """mapping between letter and number"""
-        return {"K": 1}
+        return {
+            "K": "1 0",
+            "L": "2 1",
+            "L1": "2 0",
+            "L2": "2 1",
+            "L3": "2 1",
+            "L23": "2 1",
+            "M": "3 2",
+            "M1": "3 0",
+            "M2": "3 1",
+            "M3": "3 1",
+            "M23": "3 1",
+            "M4": "3 2",
+            "M5": "3 2",
+            "M45": "3 2",
+            "N": "4 3",
+            "N1": "4 0",
+            "N2": "4 1",
+            "N3": "4 1",
+            "N23": "4 1",
+            "N4": "4 2",
+            "N5": "4 2",
+            "N45": "4 2",
+            "N6": "4 3",
+            "N7": "4 3",
+            "N67": "4 3",
+        }
 
     @staticmethod
     def _oceanKptSampling(cell, kpt):
@@ -240,7 +240,7 @@ class OCEANParameters(MSONable, _BaseParameters):
 
         cards = copy(self._cards)
 
-        cards["edges"] = f"-{element.number} {edge} 0"
+        cards["edges"] = f"-{element.number} {edge}"
         # Estimate number of band
         if self._nbands_estimator == "heg":
             eRange = self._nbands_estimator_kwargs["eRange"]
