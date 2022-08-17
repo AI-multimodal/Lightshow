897c317281d5c4511d9198c77c8c9e0e20f97d03
Matthew Carbone <x94carbone@gmail.com>
2022-08-16 22:58:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,6 @@ from setuptools import setup, find_packages
 import sys
 import versioneer
 
-
-# NOTE: This file must remain Python 2 compatible for the foreseeable future,
-# to ensure that we error out properly for people with outdated setuptools
-# and/or pip.
 min_version = (3, 7)
 if sys.version_info < min_version:
     error = """
@@ -15,13 +11,10 @@ Python {2}.{3} and above is required. Check your Python version like so:
 
 python3 --version
 
-This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
-Upgrade pip like so:
+This may be due to an out-of-date pip. Upgrade pip like so:
 
 pip install --upgrade pip
-""".format(
-        *(sys.version_info[:2] + min_version)
-    )
+""".format(*(sys.version_info[:2] + min_version))
     sys.exit(error)
 
 here = path.abspath(path.dirname(__file__))
