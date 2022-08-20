8319ed7f5a9ca37a980be62cd85b40a47edd7db4
Matthew Carbone <x94carbone@gmail.com>
2022-08-19 23:58:45 -0400

    Transition to pyproject.toml and away from setup.py/cfg

--- lightshow/__init__.py
+++ lightshow/__init__.py
@@ -1,10 +1,5 @@
 from os import environ
 
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
-
 
 def _get_API_key_from_environ():
     """Checks for an environment variable PMG_API_KEY. If does not exist,
