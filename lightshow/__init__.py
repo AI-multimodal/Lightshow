1b1ac9ea6266486f0917ee488f924126e0d0c2c1
Matthew Carbone <x94carbone@gmail.com>
2022-08-20 22:23:33 -0400

    Add working flit build

--- lightshow/__init__.py
+++ lightshow/__init__.py
@@ -1,6 +1,11 @@
 from os import environ
 
 
+# from lightshow._version import __version__  # noqa
+
+__version__ = "0.1"
+
+
 def _get_API_key_from_environ():
     """Checks for an environment variable PMG_API_KEY. If does not exist,
     returns None.
