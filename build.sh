eae9f77e276348847621cd5d4da0cd481c3f0097
Matthew Carbone <x94carbone@gmail.com>
2022-08-20 23:24:22 -0400

    Update build systems
    Semantic versioning now works. Replaces the __version__ string
    in the __init__ by using dunamai+git tags to get a valid version.

--- build.sh
+++ build.sh
@@ -1,5 +1,54 @@
 #!/bin/bash
 
-# sphinx-apidoc -f -o docs/source lightshow
-make -C docs/ html
-open docs/build/html/index.html
+build_docs () {
+
+    make -C docs/ html
+
+    # If the GitHub token is not in the environment variables, we just open
+    # the built docs in the browser. The GitHub token is present during CI
+    # runs via GitHub Actions
+    if [[ -z "${GITHUB_TOKEN}" ]]; then
+        open docs/build/html/index.html
+    fi
+    
+}
+
+replace_version_in_init () {
+    version="$(dunamai from any)"
+    sed_command="s/...  # semantic-version-placeholder/'$version'/g"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' "$sed_command" lightshow/__init__.py
+    else
+        sed -i "$sed_command" lightshow/__init__.py
+    fi
+    export _TMP_VERSION="$version"
+}
+
+reverse_replace_version_in_init () {
+    sed_command="s/'$_TMP_VERSION'/...  # semantic-version-placeholder/g"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' "$sed_command" lightshow/__init__.py
+    else
+        sed -i "$sed_command" lightshow/__init__.py
+    fi
+    unset _TMP_VERSION
+}
+
+build_flit_dev () {
+    pip install flit
+    pip install dunamai
+    replace_version_in_init
+    flit install --deps=develop
+    reverse_replace_version_in_init
+}
+
+# Iterate over all arguments in order
+for var in "$@"
+do
+    echo "Found" "$var"
+    if [ "$var" = "docs" ]; then
+        build_docs
+    elif [ "$var" = "install-dev" ]; then
+        build_flit_dev
+    fi
+done
