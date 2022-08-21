5332a24829e3f607ecae3d82a3a247641a96b0b5
Matthew Carbone <x94carbone@gmail.com>
2022-08-21 15:59:53 -0400

    Debug deployment scripts

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ replace_version_in_init () {
     else
         sed -i "$sed_command" lightshow/__init__.py
     fi
+    echo "__init__ version set to" "$version"
     export _TMP_VERSION="$version"
 }
 
@@ -18,19 +19,19 @@ reverse_replace_version_in_init () {
     else
         sed -i "$sed_command" lightshow/__init__.py
     fi
+    echo "__init__ version" "$version" "reset to placeholder"
     unset _TMP_VERSION
 }
 
 build_docs () {
 
-    replace_version_in_init
-    make -C docs/ html
-    reverse_replace_version_in_init
+    if [[ "${GITHUB_ACTION_IS_RUNNING}" = 1 ]]; then
+        flit install --deps=production --extras=doc
+    fi
 
-    # If the GitHub token is not in the environment variables, we just open
-    # the built docs in the browser. The GitHub token is present during CI
-    # runs via GitHub Actions
-    if [[ -z "${GITHUB_TOKEN}" ]]; then
+    make -C docs/ html
+
+    if [[ -z "${GITHUB_ACTION_IS_RUNNING}" ]]; then
         open docs/build/html/index.html
     fi
     
@@ -41,39 +42,31 @@ install_flit_dunamai () {
     pip install dunamai~=1.12
 }
 
-# Builds the docs from scratch
-build_CI_docs () {
-    install_flit_dunamai
-    replace_version_in_init
-    flit install --deps=production --extras=doc
-    reverse_replace_version_in_init
-    build_docs
-}
+echo "Running build script with CLI args:" "$@"
 
-build_CI_tests () {
-    install_flit_dunamai
-    replace_version_in_init
-    flit install --deps=production --extras=test
-    reverse_replace_version_in_init
-    build_docs
-}
+# Check if this is a GitHub Action. This changes the behavior of the scripts
+if [[ "${GITHUB_ACTION_IS_RUNNING}" = 1 ]]; then
+    install_flit_dunamai  # Assume we have this installed for local tests
+    echo "GitHub Action is running"
+else
+    echo "GitHub Action is not running - assuming local behavior"
+fi
 
-flit_publish () {
-    install_flit_dunamai
-    replace_version_in_init
-    flit publish --deps=production
-    reverse_replace_version_in_init
-}
+# Replace the version string placeholder in the __init__ with the real thing
+replace_version_in_init
 
 # Iterate over all arguments in order
 for var in "$@"
 do
     echo "Found" "$var"
     if [ "$var" = "docs" ]; then
-        build_CI_docs
+        build_docs
     elif [ "$var" = "test" ]; then
-        build_CI_tests
+        flit install --deps=production --extras=test
     # elif [ "$var" = "publish" ]; then
     #     flit_publish
     fi
 done
+
+# Put the version string placeholder back
+reverse_replace_version_in_init
