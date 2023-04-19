2af1ec574d54f307d5b22ba6f4e534a766d300be
Matthew Carbone <x94carbone@gmail.com>
2023-04-19 12:31:29 -0400

    Update broadening code

--- lightshow/postprocess/broaden.py
+++ lightshow/postprocess/broaden.py
@@ -1,30 +1,34 @@
 """XAS broadening using manual or empirically optimized parameters"""
 
-
-from scipy.stats import pearsonr as ps
+from monty.json import MSONable
+import numpy as np
+from scipy.stats import pearsonr
 from scipy import optimize
+from scipy.interpolate import InterpolatedUnivariateSpline
 from scipy.stats import norm
 from scipy.stats import cauchy
 from scipy.special import voigt_profile as voigt
 
-import numpy as np
 
+def gauss_broaden(xout, xin, yin, sigma):
+    """Applies Gaussian broadening to the input data, with standard deviation
+    sigma.
 
-def gauss_broaden(x, xin, yin, sigma, shift=0.0):
-    """Gaussian broadening function
+    .. math::
+
+        G(x; \\sigma) = \\frac{1}{\\sqrt{2 \\pi \\sigma^2}}
+        e^{-x^2 / 2\\sigma^2}
 
     Parameters
     ----------
-    x : numpy.ndarray
-        The output energy grid.
+    xout : numpy.ndarray
+        The output x-axis grid.
     xin : numpy.ndarray
-        The input energy grid.
+        The input x-axis grid.
     yin : numpy.ndarray
-        The input spectrum.
+        The input data for f(x) to broaden. Generally a spectrum.
     sigma : float
-        Gaussian broadening sigma.
-    shift : float
-        The energy shift.
+        The standard deviation of the Gaussian function used for broadening.
 
     Returns
     -------
@@ -32,14 +36,12 @@ def gauss_broaden(x, xin, yin, sigma, shift=0.0):
         Broadened spectrum
     """
 
-    x1, x2 = np.meshgrid(x, xin + shift)
-
-    return (
-        np.dot(norm.pdf(x1, x2, sigma).T, yin) / len(xin) * (xin[-1] - xin[0])
-    )
+    x1, x2 = np.meshgrid(xout, xin)
+    dx = xin[-1] - xin[0]
+    return np.dot(norm.pdf(x1, x2, sigma).T, yin) / len(xin) * dx
 
 
-def lorentz_broaden(x, xin, yin, gamma, shift=0.0):
+def lorentz_broaden(x, xin, yin, gamma):
     """Lorentzian broadening function
 
     Parameters
@@ -52,8 +54,6 @@ def lorentz_broaden(x, xin, yin, gamma, shift=0.0):
         The input spectrum.
     gamma : float
         Lorentzian broadening gamma.
-    shift : float
-        The energy shift.
 
     Returns
     -------
@@ -61,14 +61,12 @@ def lorentz_broaden(x, xin, yin, gamma, shift=0.0):
         Broadened spectrum.
     """
 
-    x1, x2 = np.meshgrid(x, xin + shift)
-
-    return (
-        np.dot(cauchy.pdf(x1, x2, gamma).T, yin) / len(xin) * (xin[-1] - xin[0])
-    )
+    x1, x2 = np.meshgrid(x, xin)
+    dx = xin[-1] - xin[0]
+    return np.dot(cauchy.pdf(x1, x2, gamma).T, yin) / len(xin) * dx
 
 
-def voigt_broaden(x, xin, yin, sigma, gamma, shift=0.0):
+def voigt_broaden(x, xin, yin, sigma, gamma):
     """Voigt broadening function
 
     Parameters
@@ -83,8 +81,6 @@ def voigt_broaden(x, xin, yin, sigma, gamma, shift=0.0):
         Voigt broadening sigma.
     gamma : float
         Voigt broadening gamma.
-    shift : float
-        The energy shift.
 
     Returns
     -------
@@ -92,267 +88,236 @@ def voigt_broaden(x, xin, yin, sigma, gamma, shift=0.0):
         Broadened spectrum.
     """
 
-    x1, x2 = np.meshgrid(x, xin + shift)
-
-    return (
-        np.dot(voigt(x1 - x2, sigma, gamma / 2).T, yin)
-        / len(xin)
-        * (xin[-1] - xin[0])
-    )
+    x1, x2 = np.meshgrid(x, xin)
+    dx = xin[-1] - xin[0]
+    return np.dot(voigt(x1 - x2, sigma, gamma / 2.0).T, yin) / len(xin) * dx
 
 
-class SiteSpectum:
+def energy_dependent_voigt_broaden(x, xin, yin, sigma, ld, lt, ef):
+    """See ``voigt_broaden``. Performs a similar broadening only with an
+    energy-dependent Lorentzian gamma term.
+
+    Parameters
+    ----------
+    x : numpy.ndarray
+        The output energy grid.
+    xin : numpy.ndarray
+        The input energy grid.
+    yin : numpy.ndarray
+        The input spectrum.
+    sigma : float
+        Voigt broadening sigma.
+    ld : TYPE
+        Description
+    lt : TYPE
+        Description
+    ef : TYPE
+        Description
+
+    Returns
+    -------
+    numpy.ndarray
+    """
+
+    x1, x2 = np.meshgrid(x, xin)
+    gamma = lt + np.heaviside(x2 - ef, 1) * (x2 - ef) / ld
+    dx = xin[-1] - xin[0]
+    return np.dot(voigt(x1 - x2, sigma, gamma / 2.0).T, yin) / len(xin) * dx
+
+
+def get_shift_by_maxima(exp, theory):
+    exp_x = exp[:, 0].copy()
+    exp_y = exp[:, 1].copy()
+    theory_x = theory[:, 0].copy()
+    theory_y = theory[:, 1].copy()
+
+    # Start with an initial guess: we shift the theory_x location by the
+    # difference between the location of the max experimental peak and max
+    # theory peak
+    exp_max_x = exp_x[np.argmax(exp_y)]
+    thr_max_x = theory_x[np.argmax(theory_y)]
+    return thr_max_x - exp_max_x
+
+
+def align_spectra_by_maxima(exp, theory):
+    """Performs a simple alignment of the theoretical spectrum to the
+    experiment by using the peak maxima. Returns the new x-axis.
+
+    Parameters
+    ----------
+    exp : numpy.ndarray
+    theory : numpy.ndarray
+
+    Returns
+    -------
+    numpy.ndarray
+        The aligned x-axis grid.
+    """
+
+    return theory[:, 0].copy() - get_shift_by_maxima(exp, theory)
+
+
+class SiteSpectum(MSONable):
     """This class stores information about an XAS spectrum at each site,
     including the spectrum itself, the Fermi energy and site weight.
 
     Parameters
     ----------
-    xin : numpy.ndarray
-        The input energy grid.
-    yin : numpy.ndarray
-        The input spectrum.
-    fermi : float
-        The fermi energy of the spectrum.
+    spectrum : numpy.ndarray
+        The N x 2 array corresponding to the spectrum.
+    e_fermi : float
+        The e_fermi energy of the spectrum.
     weight : int
         The weight of the site spectrum.
     """
 
-    def __init__(
-        self,
-        xin,
-        yin,
-        fermi=0.0,
-        weight=1,
-    ):
-        self._xin = xin
-        self._yin = yin
-        self._Fermi = fermi
-        self._Weight = weight
+    @property
+    def original_spectrum(self):
+        return self._original_spectrum
 
     @property
     def spectrum(self):
-        return self._xin, self._yin
+        return self._spectrum
 
     @property
     def weight(self):
-        return self._Weight
+        return self._weight
 
     @property
-    def fermi(self):
-        return self._Fermi
-
-
-class Broaden:
-    """The class that store broadening parameters and provide functions to
-    optimize broadening parameters empirically.
-
-    Parameters
-    ----------
-    sigma : float
-        The gaussian broadening sigma.
-    lorentz_divider : float
-        The parameter to determine the energy dependent gamma with equation:
-        :math:`\\Gamma\\left(E\right)=\\Gamma_0+(E-E_f)/l`,
-        where :math:`l` is the lorentz_divider, and the :math:`\\Gamma_0` is
-        determined by Corehole life time.
-    CHlifetime : float
-        Core hole life time, e.g. Zn K-edge: 1.67, Ti K-edge: 0.89.
-    shift : float
-        The energy shift.
-    """
+    def e_fermi(self):
+        return self._e_fermi
 
     def __init__(
-        self, sigma=0.0, lorentz_divider=1e5, CHlifetime=1.0, shift=0.0
-    ):
-        self._sigma = sigma
-        self._lorentz_divider = lorentz_divider
-        self._CHlifetime = CHlifetime
-        self._shift = shift
-
-    @property
-    def sigma(self):
-        return self._sigma
-
-    @sigma.setter
-    def sigma(self, sigma):
-        self._sigma = sigma
-
-    @property
-    def lorentz_divider(self):
-        return self._lorentz_divider
-
-    @lorentz_divider.setter
-    def lorentz_divider(self, lorentz_divider):
-        self._lorentz_divider = lorentz_divider
-
-    @property
-    def shift(self):
-        return self._shift
-
-    @shift.setter
-    def shift(self, shift):
-        self._shift = shift
-
-    # Do energy dependent voigt function broadening
-    @staticmethod
-    def _ene_dep_broaden(x, xin, yin, sigma, ld, lt, ef):
-        x1, x2 = np.meshgrid(x, xin)
-
-        gam = lt + np.heaviside(x2 - ef, 1) * (x2 - ef) / ld
-
-        return (
-            np.dot(voigt(x1 - x2, sigma, gam / 2).T, yin)
-            / len(xin)
-            * (xin[-1] - xin[0])
-        )
-
-    # Broaden spectrum from each site and combine them
-    def _spectrum_broaden(
-        self, x, site_spectra, sigma, ld, shift, cross_sec=True
-    ):
-        yout = np.zeros_like(x)
-
-        weight = 0
-
-        for site_spectrum in site_spectra:
-            xin, yin = site_spectrum.spectrum
-
-            xin = xin + shift
-
-            if cross_sec:
-                yin = yin * xin
-
-            yout = (
-                yout * weight
-                + self._ene_dep_broaden(
-                    x,
-                    xin,
-                    yin,
-                    sigma,
-                    ld,
-                    self._CHlifetime,
-                    site_spectrum.fermi,
-                )
-                * site_spectrum.weight
-            )
-
-            weight = weight + site_spectrum.weight
-
-        return yout / weight
-
-    # Give the pearson correlation score between broadened simulation and
-    # experimental spectrum
-    def _broaden_score(
         self,
-        broaden_paras,
-        x,
-        site_spectra,
-        exp,
-        dmu=True,
-        cross_sec=True,
-        *args,
+        spectrum,
+        original_spectrum=None,
+        e_fermi=None,
+        weight=1.0,
+        broadening_kwargs=None,
     ):
-        spec = self._spectrum_broaden(
-            x,
-            site_spectra,
-            broaden_paras[0],
-            broaden_paras[1],
-            broaden_paras[2],
-            cross_sec,
-        )
+        self._spectrum = spectrum
+        self._original_spectrum = original_spectrum
+        if self._original_spectrum is None:
+            self._original_spectrum = self._spectrum.copy()
+        self._e_fermi = e_fermi
+        self._weight = weight
+        self._broadening_kwargs = broadening_kwargs
 
-        if dmu:
-            exp = np.diff(exp) / np.diff(x)
+    def scale_max_to_1_(self):
+        self._spectrum[:, 1] /= self._spectrum[:, 1].max()
 
-            spec = np.diff(spec) / np.diff(x)
-
-        return -ps(spec, exp)[0]
-
-    def broaden(self, x, site_spectra, volume=1.0, cross_sec=True):
-        """Process broadening with the energy dependent voigt function and the
-        stored parameters.
+    def align_to_experiment_(self, exp):
+        """Modifies the spectrum in-place to align it to the provided
+        experimental result. Note that this destroys the original information
+        about the spectrum, and assumes that the original energy grid of the
+        theory data is unusable.
 
         Parameters
         ----------
-        x : numpy.ndarray
-            The output energy grid.
-        site_spectra : list
-            The list of site_spectrum objects
-        volume : float
-            The factor that tune the intensity of broadened spectrum.
-        cross_sec : bool
-            If True, the spectrum will be converted to be absorption cross
-            section.
-
-        Returns
-        -------
-        numpy.ndarray
-        """
-
-        return (
-            self._spectrum_broaden(
-                x,
-                site_spectra,
-                self._sigma,
-                self._lorentz_divider,
-                self._shift,
-                cross_sec,
-            )
-            * volume
-        )
-
-    def paras_optimize(
-        self,
-        x,
-        site_spectra,
-        exp,
-        bounds=None,
-        opt_shift=False,
-        dmu=True,
-        cross_sec=True,
-    ):
-        """Optimize the broadening parameter with simplicial homology global
-        optimization in scipy package.
-
-        Parameters
-        ----------
-        x : numpy.ndarray
-            The output energy grid.
-        site_spectra : list
-            The list of site_spectrum objects.
         exp : numpy.ndarray
-            The experimental spectrum.
-        bounds : list
-            The bounds of the broadening parameters.
-        opt_shift : bool
-            If True, the method will optimize energy shift value.
-        dmu : bool
-            If True, the method will compare the first derivatives of
-            experimental spectrum and simulation.
-        cross_sec : bool
-            If true, the spectrum will be converted to be absorption cross
-            section.
+            The experimental data to align to.
+        """
+
+        self._spectrum[:, 0] = align_spectra_by_maxima(exp, self._spectrum)
+
+    def broaden(
+        self,
+        xout,
+        method="Gaussian",
+        *broadening_args,
+        **broadening_kwargs
+    ):
+        """Returns a broadened version of the spectrum.
+
+        Parameters
+        ----------
+        xout : TYPE
+            Description
+        method : str, optional
+            The method to use. Should either match the function signature or
+            be "Gaussian" or "Lorentzian".
+        *broadening_args
+            Description
+        **broadening_kwargs
+            Extra keyword arguments to provide for the broadening. Some of
+            these will be provided. Note for "energy_dependent_voigt_broaden"
+            e_fermi is provided as an attribute.
 
         Returns
         -------
         numpy.ndarray
         """
 
-        if opt_shift is False:
-            xi = 10e-3
+        xin = self._spectrum[:, 0].copy()  # + shift
+        yin = self._spectrum[:, 1].copy()
 
-            bounds[2] = (self._shift - xi, self._shift + xi)
+        if method == "Gaussian" or method == "gauss_broaden":
+            res = gauss_broaden(
+                xout, xin, yin, *broadening_args, **broadening_kwargs
+            )
+        elif method == "Lorentzian" or method == "lorentz_broaden":
+            res = lorentz_broaden(
+                xout, xin, yin, *broadening_args, **broadening_kwargs
+            )
+        elif method == "Voigt" or method == "voigt_broaden":
+            res = voigt_broaden(
+                xout, xin, yin, *broadening_args, **broadening_kwargs
+            )
+        elif method == "energy_dependent_voigt_broaden":
+            res = energy_dependent_voigt_broaden(
+                xout,
+                xin,
+                yin,
+                *broadening_args,
+                ef=self._e_fermi,
+                **broadening_kwargs,
+            )
+        else:
+            raise ValueError(f"Uknown method {method}")
+        self._broadening_kwargs = broadening_kwargs
+        return res
 
-        result = optimize.shgo(
-            self._broaden_score,
-            bounds,
-            args=(x, site_spectra, exp, dmu, cross_sec),
-        )
+    def optimize_parameters(
+        self,
+        exp,
+        bounds,
+        method="energy_dependent_voigt_broaden",
+    ):
+        """Optimizes the broadening parameters for a single spectrum. A
+        reference spectrum (``exp``) is provided as the ground truth.
+        
+        Parameters
+        ----------
+        exp : numpy.ndarray
+            Description
+        bounds : None, optional
+            Note that shift is always the first parameter.
+        method : str, optional
+            Description
+        
+        """
 
-        self._sigma = result.x[0]
-        self._lorentz_divider = result.x[1]
+        xin_theory = self._spectrum[:, 0].copy()
 
-        if opt_shift is True:
-            self._shift = result.x[2]
+        # Bounds[0][0] is negative
+        assert bounds[0][0] <= 0.0
+        lower_shift = exp[0, 0] + bounds[0][0]
+        upper_shift = exp[-1, 0] + bounds[0][1]
 
+        self.align_to_experiment_(exp)  # makes the shift optimization easier
+
+        where_lower = np.argmin((xin_theory - lower_shift)**2)
+        where_upper = np.argmin((xin_theory - upper_shift)**2)
+
+        xin_theory = xin_theory[where_lower:where_upper]
+
+        def objective(params):
+            shift = params[0]
+            broadened_spectrum = self.broaden(xin_theory, method, *params[1:])
+            ius = InterpolatedUnivariateSpline(
+                xin_theory + shift, broadened_spectrum, k=3
+            )
+            return -pearsonr(exp[:, 1], ius(exp[:, 0]))[0]
+
+        result = optimize.shgo(objective, bounds)
         return result
