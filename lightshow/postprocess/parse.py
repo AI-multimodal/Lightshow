ea8615391d8f851be071edaf481626106a950483
chuntian236 <58525735+chuntian236@users.noreply.github.com>
2023-12-06 22:20:41 -0500

    Update parse.py

--- lightshow/postprocess/parse.py
+++ lightshow/postprocess/parse.py
@@ -1,5 +1,6 @@
 import numpy as np
-import os
+from scipy.constants import physical_constants
+import os, warnings
 from pathlib import Path
 
 def extract_FEFF(path): 
@@ -14,41 +15,26 @@ def extract_FEFF(path):
     -------
     dict_output
         A dictionary containing the spectrum. 
-        ``{"spectrum": [..., ...], "efermi": ..., "normalization_constant": ...}``
-
-    Raises
-    ------
-    OSError
-        If the output path does not contain ``xmu.dat`` file. 
+        ``{"label": "...", "energy": [...], "spectrum": [...], "efermi": ..., "normalization_constant": ...}``
     """
     
-    dict_output = {}    
+    dict_output = {} 
+    dict_output['label'] = 'cross-section'  # FEFF outputs the X-ray absorption cross-section. This label may be used for later analysis. 
     path = Path(path)
     
-    # Extract sepctrum 
-    try: 
-        spectrum = np.loadtxt(path / "xmu.dat")
-    except OSError as err: 
-        print(err)
-        return
-    dict_output['spectrum'] = np.column_stack((spectrum[:,0],spectrum[:,3]))
+    # Extract sepctrum and normalization constant
+    # The normalization constant can be used to normalize feff to absolute cross section in the unit of Angstrom^2 
+    with open(path/"xmu.dat", 'r') as f:
+        spectrum = np.loadtxt(f.readlines())
+        dict_output['energy'] = spectrum[:,0]
+        dict_output['spectrum'] = spectrum[:,3]
+        f.seek(0)
+        dict_output['normalization_constant'] = [float(line.split()[-1]) for line in f.readlines() if 'normalize mu' in line][0]
 
     # Extract Fermi energy at k=0
     efermi = spectrum[spectrum[:,2]==0,1]
-    try: 
-        dict_output['efermi'] = efermi[0]
-    except: 
-        print("Failed to extract Fermi energy.")
+    dict_output['efermi'] = efermi[0]
         
-    # Extract the normalization constant from the header
-    # This number can be used to normalize feff to absolute cross section in the unit of Angstrom^2 
-    with open(path/"xmu.dat", 'r') as f:
-        for line in f.readlines():
-            if 'normalize mu' in line:
-                normalization_constant = float(line.split()[-1])
-                dict_output['normalization_constant'] = normalization_constant
-                break
-
     return dict_output
 
 
@@ -66,50 +52,39 @@ def extract_VASP(path):
         A dictionary containing the spectrum. The spectrum is averaged from all polarization directions. 
         The volume of the super cell is extracted in the unit of Angstrom^3.
         Fermi energy and total energy are also extracted if these are contained in OUTCAR, in the unit of eV. 
-        ``{"spectrum": [..., ...], "volume": ..., "efermi": ..., "total_energy": ...}``
-
-    Raises
-    ------
-    OSError
-        If the output path does not contain ``OUTCAR`` file. 
+        ``{"label": ..., "energy": [...], "efermi": ..., "xx", [...], "yy", [...], "zz", [...], "spectrum": [...], "volume": ..., "total_energy": ...}``
     """
     
     dict_output = {}
+    dict_output['label'] = 'epsilon2'  # VASP outputs the imaginary part of the dielectric tensor. This label may be used for later analysis. 
     
     path = Path(path)
     file_outcar = path / "OUTCAR"
     volume=None
-    try: 
-        with open(file_outcar, 'r') as f:
-            for idx, line in enumerate(f.readlines()):
-                if 'volume of cell' in line and volume is None: 
-                    volume = float(line.split()[4])
-                if 'Fermi energy:' in line:
-                    efermi = float(line.split()[2])
-                if 'TOTEN' in line:
-                    total_energy = float(line.split()[-2])
-                if 'IMAGINARY DIELECTRIC FUNCTION' in line:
-                    idx_start = idx
-                if 'REAL DIELECTRIC FUNCTION' in line: 
-                    idx_end = idx
-                    break
-            f.seek(0)
-            try: 
-                lines_dielectric = f.readlines()[(idx_start+3):(idx_end-1)]
-            except: 
-                print('Wrong OUTCAR file! ')
-                return
-    except OSError as err: 
-        print(err)
-        return
+    with open(file_outcar, 'r') as f:
+        for idx, line in enumerate(f.readlines()):
+            if 'volume of cell' in line and volume is None: 
+                volume = float(line.split()[4])
+            if 'Fermi energy:' in line:
+                efermi = float(line.split()[2])
+            if 'TOTEN' in line:
+                total_energy = float(line.split()[-2])
+            if 'IMAGINARY DIELECTRIC FUNCTION' in line:
+                idx_start = idx
+            if 'REAL DIELECTRIC FUNCTION' in line: 
+                idx_end = idx
+                break
+        f.seek(0)
+        lines_dielectric = f.readlines()[(idx_start+3):(idx_end-1)]
     mu = np.array([xx.split() for xx in lines_dielectric]).astype(float)
-    mu = np.array([mu[:, 0], mu[:, 1:4].mean(axis=1)]).T
-    dict_output['spectrum'] = mu
+    dict_output['energy'] = mu[:,0]
+    dict_output['efermi'] = efermi 
+    dict_output['xx'] = mu[:,1] # xx, yy, zz are the three polarization directions
+    dict_output['yy'] = mu[:,2]
+    dict_output['zz'] = mu[:,3]
+    dict_output['spectrum'] = mu[:, 1:4].mean(axis=1)
     dict_output['volume'] = volume
-    try: dict_output['efermi'] = efermi 
-    except: pass
-    try: dict_output['total_energy'] = total_energy 
-    except: pass
+    dict_output['total_energy'] = total_energy 
 
     return dict_output
 
@@ -126,9 +101,8 @@ def extract_XSpectra(path, es_out_file=None):
     -------
     dict_output
         A dictionary containing the spectrum. The spectrum is averaged from all polarization directions in ``path``
-        Fermi level (in eV) is extracted if ``xanes.out`` file is in the directory.
-        Total energy (in eV) is extracted if the path and file name of es.out file is given. 
-        ``{"spectrum": [..., ...], "efermi": ..., "total_energy": ...}``
+        Fermi level (in eV) and Total energy (in eV) are extracted if the path and file name of es.out file is given. 
+        ``{"label": ..., "energy": [...], "spectrum": [...], "dipole1", [...], "dipole2", [...], "dipole3", [...], "efermi": ..., "total_energy": ...}``
 
     Raises
     ------
@@ -137,60 +111,35 @@ def extract_XSpectra(path, es_out_file=None):
     """
     
     dict_output = {}
+    dict_output['label'] = 'cross-section'  # XSpectra outputs the X-ray absorption cross-section. This label may be used for later analysis. 
     path = Path(path)
 
     num_polar = 0
-    spectra = None
     efermi = None
+    spectra = 0
     for sub_path in path.rglob('xanes.dat'): 
         num_polar += 1
-        if spectra is None: 
-            spectra = np.loadtxt(sub_path, usecols=(0,1))
-        else: 
-            spectra[:,1] += np.loadtxt( sub_path, usecols=(1)  )
-        ### Try to extract Fermi energy from ``xanes.out`` file. 
+        if 'energy' not in dict_output.keys(): 
+            dict_output['energy'] = np.loadtxt(sub_path, usecols=(0))
+        # Try to extract Fermi energy from ``xanes.out`` file. 
         if efermi is None: 
             file_out = Path("/".join(Path(sub_path).parts[:-1]))/"xanes.out"
-            try:
+            if file_out.exists():
                 with open(file_out, 'r') as f:
-                    for line in f.readlines():
-                        if 'ef    [eV]' in line:
-                            efermi = float(line.split()[2])
-                            break
-                try: 
-                    dict_output['efermi'] = efermi
-                except NameError as err: 
-                    print('Cannot find Fermi energy in', file_out)                            
-            except: 
-                print(file_out, 'not found. Fermi energy not extracted.')
+                    dict_output['efermi'] = [float(line.split()[2]) for line in f.readlines() if 'ef    [eV]' in line][0]
+        dict_output[sub_path.parts[-2]] = np.loadtxt( sub_path, usecols=(1)  )
+        spectra += np.loadtxt( sub_path, usecols=(1)  )
     # Raise error if there is no ``xanes.dat`` file in the directory.                         
-    try: 
-        assert num_polar > 0, "\'xanes.dat\' file not found in %s"%path 
-    except AssertionError as msg: 
-        print(msg)
-        return
+    assert num_polar > 0, "\'xanes.dat\' file not found in %s"%path 
     # Print a reminder if there are more than three polarizations in the directory. 
-    try: 
-        assert num_polar <= 3, "More than three \'xanes.dat\' files in %s. Might be a problem."%path
-    except AssertionError as msg: 
-        print(msg)
-    spectra[:,1] /= num_polar
-    dict_output['spectrum'] = spectra
+    if num_polar > 3: 
+        warnings.warn("More than three \'xanes.dat\' files in %s. Might be a problem."%path)
+    dict_output['spectrum'] = spectra/num_polar
     
     # Extract total energy if filename of es.out file is given
     if es_out_file is not None: 
-        try: 
-            with open(es_out_file, 'r') as f:
-                for line in f.readlines():
-                    if '!' in line:
-                        total_energy = float(line.split()[-2]) * 13.605703976
-                        break
-            try: 
-                dict_output['total_energy'] = total_energy
-            except NameError as err: 
-                print('Cannot find total energy in', es_out_file)                        
-        except: 
-            print(es_out_file, 'not found. Total energy not extracted.')                
+        with open(es_out_file, 'r') as f:
+            dict_output['total_energy'] = [float(line.split()[-2]) * physical_constants['Rydberg constant times hc in eV'][0] for line in f.readlines() if '!' in line][0]
 
     return dict_output
 
@@ -202,9 +151,8 @@ def extract_OCEAN(path, scf_out_file=None):
     ----------
     path : str
         OCEAN spectra output directory. 
-    scf_path: str, optional
-        OCEAN output directory where ``scf.out`` file is stored. 
-        If scf_path is not specified, will use the spectra path. 
+    scf_out_file: str, optional
+        Path of the OCEAN output file ``scf.out``.
 
     Returns
     -------
@@ -212,10 +160,13 @@ def extract_OCEAN(path, scf_out_file=None):
         A dictionary containing the spectra from different sites. Each spectrum is averaged from all polarization directions in ``path``.
         Fermi level and total energy are extracted if ``scf.out`` file is provided, with unit eV
         ``{
-            "Ti": {"0001_1s": {"spectrum": [..., ...] }, "0002_1s": {"spectrum": [..., ...] } }, 
-            "efermi": ...,
-            "total_energy": ...
+            "Ti": {
+                    "0001_1s": {"label": ..., "energy": [...], "spectrum": [...], "01": [...], "02": [...], "03": [...], "efermi": ..., "total_energy": ...}, 
+                    "0002_1s": {...},
+                    ...
+                    }, 
            }``
+        The sub-dictionary of one site is dict_output["Ti"]["0001_1s"], which has the same structure and keys as the output of the parsing functions for other codes. 
 
     Raises
     ------
@@ -224,60 +175,48 @@ def extract_OCEAN(path, scf_out_file=None):
     """
     
     dict_output = {}
-    
-    path = Path(path)
-    if scf_out_file is None:
-        scf_out_file = path / "scf.out"
-        
-    num_spectra = 0
-    spectra = None
-    for sub_path in path.rglob('absspct*'): 
-        num_spectra += 1
-        element = sub_path.parts[-1].split('.')[0].split('_')[1]
-        absorber = sub_path.parts[-1].split('.')[1].split('_')[0] + '_' + sub_path.parts[-1].split('.')[1].split('_')[1]
-        if element not in dict_output.keys(): 
-            dict_output[element] = {}
-        else: 
-            if absorber not in dict_output[element].keys(): 
-                dict_output[element][absorber] = {
-                    'spectrum': np.loadtxt( sub_path, usecols=(0,2)  ), 
-                    'num_polar': 1
-                }
-            else: 
-                dict_output[element][absorber]['spectrum'][:,1] += np.loadtxt( sub_path, usecols=(2)  )
-                dict_output[element][absorber]['num_polar'] += 1
-
-    # Raise error if there is no ``absspct...`` file in the directory.                     
-    try: 
-        assert num_spectra > 0, "\'absspct...\' file not found in %s"%path 
-    except AssertionError as msg: 
-        print(msg)
-        return
-    
-    for element in dict_output.keys(): 
-        for absorber in dict_output[element].keys():
-            dict_output[element][absorber]['spectrum'][:,1] /= dict_output[element][absorber]['num_polar']
-            del dict_output[element][absorber]['num_polar']
 
     # Try to extract Fermi energy from ``scf.out`` file. 
-    file_out = scf_out_file
-    try: 
-        with open(file_out, 'r') as f:
+    efermi = None
+    total_energy = None
+    if scf_out_file is not None: 
+        with open(scf_out_file, 'r') as f:
             for line in f.readlines():
                 if 'Fermi energy' in line:
                     efermi = float(line.split()[4])
                 if '!' in line:
-                    total_energy = float(line.split()[-2]) * 13.605703976
-        try: 
-            dict_output['efermi'] = efermi
-        except NameError as err: 
-            print('Cannot find Fermi energy in', file_out)
-        try: 
-            dict_output['total_energy'] = total_energy
-        except: 
-            print('Cannot find total energy in', file_out) 
-    except OSError: 
-        print(file_out, 'not found. Fermi energy and total energy not extracted.')
+                    total_energy = float(line.split()[-2]) * physical_constants['Rydberg constant times hc in eV'][0]
+
+    path = Path(path)    
+    num_spectra = 0
+    for sub_path in path.rglob('absspct*'): 
+        num_spectra += 1
+        element = sub_path.parts[-1].split('.')[0].split('_')[1]
+        absorber = sub_path.parts[-1].split('.')[1].split('_')[0] + '_' + sub_path.parts[-1].split('.')[1].split('_')[1]
+        polarization = sub_path.parts[-1].split('.')[1].split('_')[-1]
+        if element not in dict_output.keys(): 
+            dict_output[element] = {}
+        if absorber not in dict_output[element].keys(): 
+            dict_output[element][absorber] = {
+                'label': 'epsilon2',  # OCEAN outputs the imaginary part of the dielectric tensor. This label may be used for later analysis. 
+                'energy': np.loadtxt( sub_path, usecols=(0)  ), 
+                'num_polar': 1
+            }
+            if efermi is not None: dict_output[element][absorber]['efermi'] = efermi
+            if total_energy is not None: dict_output[element][absorber]['total_energy'] = total_energy
+            dict_output[element][absorber]['spectrum'] = np.loadtxt( sub_path, usecols=(2)  )
+        else: 
+            dict_output[element][absorber]['spectrum'] += np.loadtxt( sub_path, usecols=(2)  )
+            dict_output[element][absorber]['num_polar'] += 1
+        dict_output[element][absorber][polarization] = np.loadtxt( sub_path, usecols=(2)  )
+        
+    # Raise error if there is no ``absspct...`` file in the directory.                     
+    assert num_spectra > 0, "\'absspct...\' file not found in %s"%path 
+    
+    for element in dict_output.keys(): 
+        for absorber in dict_output[element].keys():
+            dict_output[element][absorber]['spectrum'] /= dict_output[element][absorber]['num_polar']
+            del dict_output[element][absorber]['num_polar']
     
     return dict_output
 
@@ -289,16 +228,15 @@ def extract_exciting(path, INFO_out_file=None):
     ----------
     path : str
         exciting spectra output directory. 
-    INFO_path: str, optional
-        exciting output directory where ``INFO.OUT`` file is stored. 
-        If INFO_path is not specified, will use the spectra path. 
+    INFO_out_file: str, optional
+        Path of the exciting output file ``INFO.out``. 
         
     Returns
     -------
     dict_output
         A dictionary containing the spectrum. The spectrum is averaged from all polarization directions in ``path``.
         Fermi level and total energy are extracted if ``INFO.OUT`` file is provided, with unit eV.
-        ``{"spectrum": [..., ...], "efermi": ..., "total_energy": ...}``
+        ``{"label": ..., "energy": [...], "spectrum": [...], "11": [...], "22": [...], "33": [...], "efermi": ..., "total_energy": ...}``
 
     Raises
     ------
@@ -307,55 +245,36 @@ def extract_exciting(path, INFO_out_file=None):
     """
     
     dict_output = {}
-    
+    dict_output['label'] = 'epsilon2'  # EXCITING outputs the imaginary part of the dielectric tensor. This label may be used for later analysis. 
     path = Path(path)
-    if INFO_out_file is None:
-        INFO_out_file = path / "INFO.OUT"
     
     num_polar = 0
-    spectra = None
+    spectra = 0
     for sub_path in path.rglob('EPSILON*OUT'): 
         num_polar += 1
-        if spectra is None: 
-            spectra = np.loadtxt(sub_path, usecols=(0,2))
-        else: 
-            spectra[:,1] += np.loadtxt( sub_path, usecols=(2)  )
-    ### Raise error if there is no ``EPSILON...OUT`` file in the directory.                     
-    try: 
-        assert num_polar > 0, "\'EPSILON...OUT\' file not found in %s"%path 
-    except AssertionError as msg: 
-        print(msg)
-        return
+        if 'energy' not in dict_output.keys(): 
+            dict_output['energy'] = np.loadtxt(sub_path, usecols=(0))
+        polarization = ''.join( [ii for ii in sub_path.parts[-1].split('.')[0].split('_')[-1] if ii.isdigit()] )
+        dict_output[polarization] = np.loadtxt( sub_path, usecols=(2)  )
+        spectra += np.loadtxt( sub_path, usecols=(2)  )
+
+    # Raise error if there is no ``EPSILON...OUT`` file in the directory.                     
+    assert num_polar > 0, "\'EPSILON...OUT\' file not found in %s"%path 
     # Print a reminder if there are more than three polarizations in the directory. 
-    try: 
-        assert num_polar <= 3, "More than three \'EPSILON...OUT\' files in %s. Might be a problem."%path
-    except AssertionError as msg: 
-        print(msg)
-        
-    spectra[:,1] /= num_polar
-    dict_output['spectrum'] = spectra
+    if num_polar > 3:
+        warnings.warn("More than three \'EPSILON...OUT\' files in %s. Might be a problem."%path)
+    dict_output['spectrum'] = spectra/num_polar
 
     # Extract Fermi energy and total energy from ``INFO.OUT`` file. 
-    file_out = INFO_out_file
-    lines_fermi = []
-    try: 
-        with open(file_out, 'r') as f:
+    if INFO_out_file is not None:
+        lines_fermi = []
+        with open(INFO_out_file, 'r') as f:
             for line in f.readlines():
                 if 'Fermi energy' in line:
                     lines_fermi.append(line)
                 if 'Total energy' in line:
-                    total_energy = float(line.split()[3])*27.211407953
-        try: 
-            dict_output['efermi'] = float(lines_fermi[-2].split()[3])*27.211407953
-        except: 
-            print('Cannot find Fermi energy in', file_out) 
-        try: 
+                    total_energy = float(line.split()[3])*physical_constants['Hartree energy in eV'][0]
+            dict_output['efermi'] = float(lines_fermi[-2].split()[3])*physical_constants['Hartree energy in eV'][0]
             dict_output['total_energy'] = total_energy
-        except: 
-            print('Cannot find total energy in', file_out) 
-    except: 
-        print(file_out, 'not found. Fermi energy and total energy not extracted.')
 
     return dict_output
-
-
