501964cf4df8e185e66082bcd1983084aa8d8da1
Matthew Carbone <x94carbone@gmail.com>
2024-11-21 11:47:44 -0500

    Update build script to use uv

--- scripts/build_project.sh
+++ scripts/build_project.sh
@@ -2,5 +2,5 @@
 
 bash scripts/install.sh build
 echo "__version__ = '$(dunamai from any --style=pep440 --no-metadata)'" >lightshow/_version.py
-flit build
+uv build --package lightshow
 git checkout lightshow/_version.py
