90739d252a3936a6d43fe882a18d04080c8fb6b5
Xiaohui Qu <xq@earth.org>
2025-01-09 13:38:32 -0500

    make the dropdown not clearable

--- lightshow/ai/xas_ui.py
+++ lightshow/ai/xas_ui.py
@@ -32,7 +32,7 @@ ene_start = {'Ti': 4964.504, 'V': 5464.097, 'Cr': 5989.168, 'Mn': 6537.886,
              'Fe': 7111.23, 'Co': 7709.282, 'Ni': 8332.181, 'Cu': 8983.173}
 ene_grid = {el: np.linspace(start, start + 35, 141) for el, start in ene_start.items()}
 xas_model_names = [f'{el} FEFF' for el in all_elements] + ['Ti VASP', 'Cu VASP']
-absorber_dropdown = dcc.Dropdown(xas_model_names, value='Ti VASP', id='absorber')
+absorber_dropdown = dcc.Dropdown(xas_model_names, clearable=False, value='Ti VASP', id='absorber')
 
 onmixas_layout = Columns([
         Column(Box([Loading(search_component.layout()),
